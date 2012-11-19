b5f25b03293aebe617d989a37e9dee0da2a1d3ed
veeck <michael.veeck@gmx.net>
2012-11-20 00:09:49 +0100

    ongoing work

--- js/wowfeed.js
+++ js/wowfeed.js
@@ -1,36 +1,78 @@
 var http = require('http'),
     rss = require('rss'),
+    url = require('url'),
     qs = require('querystring'),
     htmlparser = require('htmlparser');
 
 /////////// Create and start the server to handle requests
 http.createServer(function (request, response)
 {
-    if (request.url.indexOf('character') == -1) {
+    // Extract the searchquery from the querystring
+    var url_parts = url.parse(request.url,true);
+    var character = url_parts.query.character;
+    var realm = url_parts.query.realm;
+    var region = url_parts.query.region;
+
+    if (!region || !realm || !character) {
+        // Tell the client the search params were not correct
         response.writeHead(200, {'Content-Type':'text/html'});
-        response.end('Invalid call, please specify character\n');
+        response.end('Invalid call, please specify character, realm and region!\n');
     }
     else {
-        //tell the client that return value is of rss type
+        // Tell the client that return value is of rss type
         response.writeHead(200, {'Content-Type':'application/rss+xml'});
-
-        //extract the searchquery from the querystring.. this is a REST service
-        var str_querystring = request.url.substring(request.url.indexOf('character'));
-        var character = qs.parse(str_querystring).character;
-        var realm = 'khazgoroth';
-        console.log('\nsearch char:' + qs.parse(str_querystring).character);
-
-        //method call to process the search and print rss
-        processquery(realm, character, response);
+        processquery(region, realm, character, response);
     }
 }).listen(8080);
 
 console.log('Server running at http://localhost:8080/');
 
-function processquery(realm, character, responseObj)
+function processitem(item)
+{
+    var rss = {};
+    rss.date = item.timestamp;
+    rss.categories = [item.type];
+
+    rss.description = 'todo desc';
+    rss.title = item.type;
+    rss.guid = 0;
+    rss.author = 'todo author';
+    rss.url = 'todo url';
+
+    switch (item.type)
+    {
+        case ("ACHIEVEMENT"):
+            rss.title = item.achievement.title;
+            rss.description = "Earned the achievement '" + item.achievement.description +"' for " + item.achievement.points +" points.";
+            break;
+
+        case ("CRITERIA"):
+            rss.title = item.achievement.title;
+            rss.description = "Completed step '" + item.criteria.description + "' of achievement '" + item.achievement.description +"'.";
+            break;
+
+        case ("LOOT"):
+            rss.title = "Loot";
+            rss.description = "Obtained '" + item.itemId +"'.";
+            break;
+
+        case ("BOSSKILL"):
+            rss.title = item.name;
+            rss.description = item.quantity + " " + item.achievement.title;
+            break;
+
+        default:
+            console.log("Unhandled type: " + item.type);
+            break;
+    }
+
+    return rss;
+}
+
+function processquery(region, realm, character, responseObj)
 {
     var options = {
-        host:'eu.battle.net',
+        host:region+'.battle.net',
         path:'/api/wow/character/'+realm+'/'+character+'?fields=feed'
     };
 
@@ -52,15 +94,7 @@ function processquery(realm, character, responseObj)
 
             // Loop over data and add to feed
             js.feed.forEach(function (item) {
-                feed.item({
-                    description: 'todo desc',
-                    title:item.type,
-                    guid: 0,
-                    date: item.timestamp,
-                    author: 'todo author',
-                    categories: [item.type],
-                    url: 'todo url'
-                });
+                feed.item(processitem(item));
             });
 
             //Print the RSS feed out as response
