6cb02e956730d308d945b1d0652e677c46689969
Michael Veeck <michael@veeck.de>
2012-11-19 18:19:36 +0100

    updated to produce basic rss

--- js/wowfeed.js
+++ js/wowfeed.js
@@ -1,64 +1,65 @@
 var http = require('http'),
     rss = require('rss'),
     qs = require('querystring'),
-    htmlparser = require("htmlparser");
-
+    htmlparser = require('htmlparser');
 
 /////////// Create and start the server to handle requests
 http.createServer(function (request, response)
 {
-    if (request.url.indexOf('searchcp') == -1)
-    {
+    if (request.url.indexOf('character') == -1) {
         response.writeHead(200, {'Content-Type':'text/html'});
-        response.end('Invalid call\n');
-    } else {
+        response.end('Invalid call, please specify character\n');
+    }
+    else {
         //tell the client that return value is of rss type
         response.writeHead(200, {'Content-Type':'application/rss+xml'});
 
         //extract the searchquery from the querystring.. this is a REST service
-        var str_querystring = request.url.substring(request.url.indexOf('searchcp'));
-        var searchString = qs.parse(str_querystring).searchcp;
-        console.log('\nsearch string:' + qs.parse(str_querystring).searchcp);
+        var str_querystring = request.url.substring(request.url.indexOf('character'));
+        var character = qs.parse(str_querystring).character;
+        var realm = 'khazgoroth';
+        console.log('\nsearch char:' + qs.parse(str_querystring).character);
 
         //method call to process the search and print rss
-        processquery(searchString, response);
+        processquery(realm, character, response);
     }
 }).listen(8080);
 
 console.log('Server running at http://localhost:8080/');
 
-function processquery(searchString, responseObj)
+function processquery(realm, character, responseObj)
 {
-    // code to get xml
     var options = {
         host:'eu.battle.net',
-        port:80,
-        path:'/api/wow/character/khazgoroth/frchorknabe?fields=feed',
-        method:'GET'
+        path:'/api/wow/character/'+realm+'/'+character+'?fields=feed'
     };
 
     var handler = new htmlparser.DefaultHandler(function (error, dom) {
-        if (error) { }
-        else
-        {
-            // Parse JSOn we get from blizz
-            var js = JSON.parse(dom[0].data);
-
-            //  Generate RSS feed
+        if (error) {
+        }
+        else {
+            ///////////// Generate RSS feed
             var feed = new rss({
-                title:'Codeproject search result Sample RSS Feed',
-                description:'Code project search Results RSS feed through node.js sample',
-                feed_url:'http://codeproject.com',
-                site_url:'http://codeproject.com',
-                author:'You'
+                title:'RSS feed for '+character+' on '+realm,
+                description:'RSS feed generated from blizzards json feed-api',
+                feed_url:'http://'+options.host+options.path,
+                site_url:'http://'+options.host+'/wow/character/'+realm+'/'+character+'/feed',
+                author:'rejas'
             });
 
+            // Parse JSON we get from blizzard
+            var js = JSON.parse(dom[0].data);
+
             // Loop over data and add to feed
-            js.feed.forEach(function (item)
-            {
+            js.feed.forEach(function (item) {
                 feed.item({
+                    description: 'todo desc',
                     title:item.type,
-                    url:'http://www.codeproject.com' + item.URL
+                    guid: 0,
+                    date: item.timestamp,
+                    author: 'todo author',
+                    categories: [item.type],
+                    url: 'todo url'
                 });
             });
 
@@ -72,11 +73,13 @@ function processquery(searchString, responseObj)
     var req = http.request(options, function (res) {
         console.log('STATUS: ' + res.statusCode);
         //console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
 
         var alldata = "";
         res.on('data', function (chunk) {
             alldata = alldata + chunk;
+        });
+        res.on('error', function (e) {
+            console.log('problem with request: ' + e.message);
         });
         res.on('end', function () {
             html_parser.parseComplete(alldata);
@@ -87,10 +90,5 @@ function processquery(searchString, responseObj)
         console.log('problem with request: ' + e.message);
     });
 
-    req.write('data\n');
     req.end();
-}
-
-function print(value) {
-    console.log('\n' + value);
 }
