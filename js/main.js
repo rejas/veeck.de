f691be6073ed9c99c994e952368b949cb0ca9a5c
veeck <michael@veeck.de>
2013-11-20 23:49:31 +0100

    udapte test page

--- js/main.js
+++ js/main.js
@@ -65,4 +65,17 @@ $(document).ready(function () {
             }
         });
     }
+
+    var $tc = $('#textContainer');
+    var $tc2 = $('<div class="cb"></div>');
+    $("body").append($tc2);
+
+    while ($tc.overflown()) {
+        $tc.children().last().prependTo($tc2);
+    }
 });
+
+$.fn.overflown=function(){
+    var e = this[0];
+    return e.scrollHeight>e.clientHeight||e.scrollWidth>e.clientWidth;
+}
