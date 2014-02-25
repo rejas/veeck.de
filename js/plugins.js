e174450391ab90543965e0b9302d01c779ad59a1
Veeck <veeck@neofonie.de>
2014-02-25 15:59:22 +0100

    put dropdown js in plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -740,256 +740,216 @@
     $.fn.cropresize = $.fn.resizecrop; // -- deprecated, Backward compatibility
 })(jQuery);
 
-
 /**
- * jquery.dlmenu.js v1.0.1
+ * jquery.dropdown.js v1.0.0
  * http://www.codrops.com
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
  *
- * Copyright 2013, Codrops
+ * Copyright 2012, Codrops
  * http://www.codrops.com
  */
 ( function( $, window, undefined ) {
 
     'use strict';
 
-    // global
-    var Modernizr = window.Modernizr, $body = $( 'body' );
-
-    $.DLMenu = function( options, element ) {
+    $.DropDown = function( options, element ) {
         this.$el = $( element );
         this._init( options );
     };
 
     // the options
-    $.DLMenu.defaults = {
-        // classes for the animation effects
-        animationClasses : { classin : 'dl-animate-in-1', classout : 'dl-animate-out-1' },
-        // callback: click a link that has a sub menu
-        // el is the link element (li); name is the level name
-        onLevelClick : function( el, name ) { return false; },
-        // callback: click a link that does not have a sub menu
-        // el is the link element (li); ev is the event obj
-        onLinkClick : function( el, ev ) { return false; }
+    $.DropDown.defaults = {
+        speed : 300,
+        easing : 'ease',
+        gutter : 0,
+        // initial stack effect
+        stack : true,
+        // delay between each option animation
+        delay : 0,
+        // random angle and positions for the options
+        random : false,
+        // rotated [right||left||false] : the options will be rotated to thr right side or left side.
+        // make sure to tune the transform origin in the stylesheet
+        rotated : false,
+        // effect to slide in the options. value is the margin to start with
+        slidingIn : false,
+        // z-index in case you have overlay problems with multiple selects
+        zindex: 1000,
+        //
+        onOptionSelect : function(opt) { return false; }
     };
 
-    $.DLMenu.prototype = {
+    $.DropDown.prototype = {
+
         _init : function( options ) {
 
             // options
-            this.options = $.extend( true, {}, $.DLMenu.defaults, options );
-            // cache some elements and initialize some variables
-            this._config();
-
-            var animEndEventNames = {
-                    'WebkitAnimation' : 'webkitAnimationEnd',
-                    'OAnimation' : 'oAnimationEnd',
-                    'msAnimation' : 'MSAnimationEnd',
-                    'animation' : 'animationend'
-                },
-                transEndEventNames = {
-                    'WebkitTransition' : 'webkitTransitionEnd',
-                    'MozTransition' : 'transitionend',
-                    'OTransition' : 'oTransitionEnd',
-                    'msTransition' : 'MSTransitionEnd',
-                    'transition' : 'transitionend'
-                };
-            // animation end event name
-            this.animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ] + '.dlmenu';
-            // transition end event name
-            this.transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ] + '.dlmenu',
-                // support for css animations and css transitions
-                this.supportAnimations = Modernizr.cssanimations,
-                this.supportTransitions = Modernizr.csstransitions;
-
+            this.options = $.extend( true, {}, $.DropDown.defaults, options );
+            this._layout();
             this._initEvents();
 
         },
-        _config : function() {
-            this.open = false;
-            this.$trigger = this.$el.children( '.dl-trigger' );
-            this.$menu = this.$el.children( 'ul.dl-menu' );
-            this.$menuitems = this.$menu.find( 'li:not(.dl-back)' );
-            this.$el.find( 'ul.dl-submenu' ).prepend( '<li class="dl-back"><a href="#">back</a></li>' );
-            this.$back = this.$menu.find( 'li.dl-back' );
+        _layout : function() {
+
+            var self = this;
+            this.minZIndex = this.options.zindex;
+            var value = this._transformSelect();
+            this.opts = this.listopts.children( 'li' );
+            this.optsCount = this.opts.length;
+            this.size = { width : this.dd.width(), height : this.dd.height() };
+
+            var elName = this.$el.attr( 'name' ), elId = this.$el.attr( 'id' ),
+                inputName = elName !== undefined ? elName : elId !== undefined ? elId : 'cd-dropdown-' + ( new Date() ).getTime();
+
+            this.inputEl = $( '<input type="hidden" name="' + inputName + '" value="' + value + '"></input>' ).insertAfter( this.selectlabel );
+
+            this.selectlabel.css( 'z-index', this.minZIndex + this.optsCount );
+            this._positionOpts();
+            if( Modernizr.csstransitions ) {
+                setTimeout( function() { self.opts.css( 'transition', 'all ' + self.options.speed + 'ms ' + self.options.easing ); }, 25 );
+            }
+
+        },
+        _transformSelect : function() {
+
+            var optshtml = '', selectlabel = '', value = -1;
+            this.$el.children( 'option' ).each( function() {
+
+                var $this = $( this ),
+                    val = isNaN( $this.attr( 'value' ) ) ? $this.attr( 'value' ) : Number( $this.attr( 'value' ) ) ,
+                    classes = $this.attr( 'class' ),
+                    selected = $this.attr( 'selected' ),
+                    label = $this.text();
+
+                if( val !== -1 ) {
+                    optshtml +=
+                        classes !== undefined ?
+                            '<li data-value="' + val + '"><span class="' + classes + '">' + label + '</span></li>' :
+                            '<li data-value="' + val + '"><span>' + label + '</span></li>';
+                }
+
+                if( selected ) {
+                    selectlabel = label;
+                    value = val;
+                }
+
+            } );
+
+            this.listopts = $( '<ul/>' ).append( optshtml );
+            this.selectlabel = $( '<span/>' ).append( selectlabel );
+            this.dd = $( '<div class="cd-dropdown"/>' ).append( this.selectlabel, this.listopts ).insertAfter( this.$el );
+            this.$el.remove();
+
+            return value;
+
+        },
+        _positionOpts : function( anim ) {
+
+            var self = this;
+
+            this.listopts.css( 'height', 'auto' );
+            this.opts
+                .each( function( i ) {
+                    $( this ).css( {
+                        zIndex : self.minZIndex + self.optsCount - 1 - i,
+                        top : self.options.slidingIn ? ( i + 1 ) * ( self.size.height + self.options.gutter ) : 0,
+                        left : 0,
+                        marginLeft : self.options.slidingIn ? i % 2 === 0 ? self.options.slidingIn : - self.options.slidingIn : 0,
+                        opacity : self.options.slidingIn ? 0 : 1,
+                        transform : 'none'
+                    } );
+                } );
+
+            if( !this.options.slidingIn ) {
+                this.opts
+                    .eq( this.optsCount - 1 )
+                    .css( { top : this.options.stack ? 9 : 0, left : this.options.stack ? 4 : 0, width : this.options.stack ? this.size.width - 8 : this.size.width, transform : 'none' } )
+                    .end()
+                    .eq( this.optsCount - 2 )
+                    .css( { top : this.options.stack ? 6 : 0, left : this.options.stack ? 2 : 0, width : this.options.stack ? this.size.width - 4 : this.size.width, transform : 'none' } )
+                    .end()
+                    .eq( this.optsCount - 3 )
+                    .css( { top : this.options.stack ? 3 : 0, left : 0, transform : 'none' } );
+            }
+
         },
         _initEvents : function() {
 
             var self = this;
 
-            this.$trigger.on( 'click.dlmenu', function() {
-
-                if( self.open ) {
-                    self._closeMenu();
-                }
-                else {
-                    self._openMenu();
-                }
+            this.selectlabel.on( 'mousedown.dropdown', function( event ) {
+                self.opened ? self.close() : self.open();
                 return false;
 
             } );
 
-            this.$menuitems.on( 'click.dlmenu', function( event ) {
-
-                event.stopPropagation();
-
-                var $item = $(this),
-                    $submenu = $item.children( 'ul.dl-submenu' );
-
-                if( $submenu.length > 0 ) {
-
-                    var $flyin = $submenu.clone().css( 'opacity', 0 ).insertAfter( self.$menu ),
-                        onAnimationEndFn = function() {
-                            self.$menu.off( self.animEndEventName ).removeClass( self.options.animationClasses.classout ).addClass( 'dl-subview' );
-                            $item.addClass( 'dl-subviewopen' ).parents( '.dl-subviewopen:first' ).removeClass( 'dl-subviewopen' ).addClass( 'dl-subview' );
-                            $flyin.remove();
-                        };
-
-                    setTimeout( function() {
-                        $flyin.addClass( self.options.animationClasses.classin );
-                        self.$menu.addClass( self.options.animationClasses.classout );
-                        if( self.supportAnimations ) {
-                            self.$menu.on( self.animEndEventName, onAnimationEndFn );
-                        }
-                        else {
-                            onAnimationEndFn.call();
-                        }
-
-                        self.options.onLevelClick( $item, $item.children( 'a:first' ).text() );
-                    } );
-
-                    return false;
-
+            this.opts.on( 'click.dropdown', function() {
+                if( self.opened ) {
+                    var opt = $( this );
+                    self.options.onOptionSelect( opt );
+                    self.inputEl.val( opt.data( 'value' ) );
+                    self.selectlabel.html( opt.html() );
+                    self.close();
                 }
-                else {
-                    self.options.onLinkClick( $item, event );
-                }
-
             } );
 
-            this.$back.on( 'click.dlmenu', function( event ) {
+        },
+        open : function() {
+            var self = this;
+            this.dd.toggleClass( 'cd-active' );
+            this.listopts.css( 'height', ( this.optsCount + 1 ) * ( this.size.height + this.options.gutter ) );
+            this.opts.each( function( i ) {
 
-                var $this = $( this ),
-                    $submenu = $this.parents( 'ul.dl-submenu:first' ),
-                    $item = $submenu.parent(),
-
-                    $flyin = $submenu.clone().insertAfter( self.$menu );
-
-                var onAnimationEndFn = function() {
-                    self.$menu.off( self.animEndEventName ).removeClass( self.options.animationClasses.classin );
-                    $flyin.remove();
-                };
-
-                setTimeout( function() {
-                    $flyin.addClass( self.options.animationClasses.classout );
-                    self.$menu.addClass( self.options.animationClasses.classin );
-                    if( self.supportAnimations ) {
-                        self.$menu.on( self.animEndEventName, onAnimationEndFn );
-                    }
-                    else {
-                        onAnimationEndFn.call();
-                    }
-
-                    $item.removeClass( 'dl-subviewopen' );
-
-                    var $subview = $this.parents( '.dl-subview:first' );
-                    if( $subview.is( 'li' ) ) {
-                        $subview.addClass( 'dl-subviewopen' );
-                    }
-                    $subview.removeClass( 'dl-subview' );
+                $( this ).css( {
+                    opacity : 1,
+                    top : self.options.rotated ? self.size.height + self.options.gutter : ( i + 1 ) * ( self.size.height + self.options.gutter ),
+                    left : self.options.random ? Math.floor( Math.random() * 11 - 5 ) : 0,
+                    width : self.size.width,
+                    marginLeft : 0,
+                    transform : self.options.random ?
+                        'rotate(' + Math.floor( Math.random() * 11 - 5 ) + 'deg)' :
+                        self.options.rotated ?
+                            self.options.rotated === 'right' ?
+                                'rotate(-' + ( i * 5 ) + 'deg)' :
+                                'rotate(' + ( i * 5 ) + 'deg)'
+                            : 'none',
+                    transitionDelay : self.options.delay && Modernizr.csstransitions ? self.options.slidingIn ? ( i * self.options.delay ) + 'ms' : ( ( self.optsCount - 1 - i ) * self.options.delay ) + 'ms' : 0
                 } );
 
-                return false;
-
             } );
+            this.opened = true;
 
         },
-        closeMenu : function() {
-            if( this.open ) {
-                this._closeMenu();
-            }
-        },
-        _closeMenu : function() {
-            var self = this,
-                onTransitionEndFn = function() {
-                    self.$menu.off( self.transEndEventName );
-                    self._resetMenu();
-                };
+        close : function() {
 
-            this.$menu.removeClass( 'dl-menuopen' );
-            this.$menu.addClass( 'dl-menu-toggle' );
-            this.$trigger.removeClass( 'dl-active' );
-
-            if( this.supportTransitions ) {
-                this.$menu.on( this.transEndEventName, onTransitionEndFn );
-            }
-            else {
-                onTransitionEndFn.call();
-            }
-
-            this.open = false;
-        },
-        openMenu : function() {
-            if( !this.open ) {
-                this._openMenu();
-            }
-        },
-        _openMenu : function() {
             var self = this;
-            // clicking somewhere else makes the menu close
-            $body.off( 'click' ).on( 'click.dlmenu', function() {
-                self._closeMenu() ;
-            } );
-            this.$menu.addClass( 'dl-menuopen dl-menu-toggle' ).on( this.transEndEventName, function() {
-                $( this ).removeClass( 'dl-menu-toggle' );
-            } );
-            this.$trigger.addClass( 'dl-active' );
-            this.open = true;
-        },
-        // resets the menu to its original state (first level of options)
-        _resetMenu : function() {
-            this.$menu.removeClass( 'dl-subview' );
-            this.$menuitems.removeClass( 'dl-subview dl-subviewopen' );
+            this.dd.toggleClass( 'cd-active' );
+            if( this.options.delay && Modernizr.csstransitions ) {
+                this.opts.each( function( i ) {
+                    $( this ).css( { 'transition-delay' : self.options.slidingIn ? ( ( self.optsCount - 1 - i ) * self.options.delay ) + 'ms' : ( i * self.options.delay ) + 'ms' } );
+                } );
+            }
+            this._positionOpts( true );
+            this.opened = false;
         }
     };
 
-    var logError = function( message ) {
-        if ( window.console ) {
-            window.console.error( message );
-        }
-    };
-
-    $.fn.dlmenu = function( options ) {
+    $.fn.dropdown = function( options ) {
+        var instance = $.data( this, 'dropdown' );
         if ( typeof options === 'string' ) {
             var args = Array.prototype.slice.call( arguments, 1 );
             this.each(function() {
-                var instance = $.data( this, 'dlmenu' );
-                if ( !instance ) {
-                    logError( "cannot call methods on dlmenu prior to initialization; " +
-                        "attempted to call method '" + options + "'" );
-                    return;
-                }
-                if ( !$.isFunction( instance[options] ) || options.charAt(0) === "_" ) {
-                    logError( "no such method '" + options + "' for dlmenu instance" );
-                    return;
-                }
                 instance[ options ].apply( instance, args );
             });
         }
         else {
             this.each(function() {
-                var instance = $.data( this, 'dlmenu' );
-                if ( instance ) {
-                    instance._init();
-                }
-                else {
-                    instance = $.data( this, 'dlmenu', new $.DLMenu( options, this ) );
-                }
+                instance ? instance._init() : instance = $.data( this, 'dropdown', new $.DropDown( options, this ) );
             });
         }
-        return this;
+        return instance;
     };
 
 } )( jQuery, window );
