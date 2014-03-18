2b102c036c2b09ec04d3ee7ecc2f097c0753517b
veeck <michael@veeck.de>
2014-03-18 13:11:38 +0100

    re-add live watch to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,13 @@ var uglify  = require('gulp-uglify');
 var es      = require('event-stream');
 var stylish  = require('jshint-stylish');
 
+var gutil   = require('gulp-util');
+var embedlr = require('gulp-embedlr');
+var refresh = require('gulp-livereload');
+var express = require('express');
+var http    = require('http');
+var lr      = require('tiny-lr')();
+
 gulp.task('clean', function () {
     // Clear the destination folder
     gulp.src('dist/**/*.*', { read: false })
@@ -71,5 +78,55 @@ gulp.task('html', function() {
         }))
         .pipe(gulp.dest("./dist"));
 });
+
+gulp.task('server', function () {
+    // Create a HTTP server for static files
+    var port = 3000;
+    var app = express();
+    var server = http.createServer(app);
+
+    app.use(express.static(__dirname + '/dist'));
+
+    server.on('listening', function () {
+        gutil.log('Listening on http://locahost:' + server.address().port);
+    });
+
+    server.on('error', function (err) {
+        if (err.code === 'EADDRINUSE') {
+            gutil.log('Address in use, retrying...');
+            setTimeout(function () {
+                server.listen(port);
+            }, 1000);
+        }
+    });
+
+    server.listen(port);
+});
+
+gulp.task('lr-server', function () {
+    // Create a LiveReload server
+    lr.listen(35729, function (err) {
+        if (err) {
+            gutil.log(err);
+        }
+    });
+});
+
+gulp.task('watch', function () {
+    // Watch .js files and run tasks if they change
+    gulp.watch('src/js/**/*.js', ['scripts']);
+
+    // Watch .less files and run tasks if they change
+    gulp.watch('src/less/**/*.less', ['styles', 'html']);
+
+    /*
+    gulp.src('./src/*.html')
+        .pipe(embedlr())
+        .pipe(gulp.dest('./dist'));
+    */
+});
+
+// The default task (called when you run `gulp`)
+gulp.task('local', ['clean', 'copy', 'scripts', 'styles', 'html', 'lr-server', 'server', 'watch']);
 
 gulp.task('default', ['clean', 'copy', 'scripts', 'styles', 'html']);
