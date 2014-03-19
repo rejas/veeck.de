2154d2d178ce45360b24f98fbb1b968a3bbce466
veeck <michael@veeck.de>
2014-03-19 13:28:32 +0100

    small cleanups

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -316,7 +316,7 @@
             $('.acitem', this).hide();
             $('li.expand > .acitem', this).show();
             $('li.expand > .acitem', this).prev().addClass('active');
-            $('#menuButton').on ('click', function (e) {
+            $('#menuButton').on (settings.action, function (e) {
                 $('#desktopmenu').show();
             });
             $('li a', this).on (settings.action, function (e)
