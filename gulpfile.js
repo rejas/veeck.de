81e63059ca3310d801cc16ecd98c6651cb760efd
veeck <michael@veeck.de>
2014-03-23 18:12:25 +0100

    fix gulp js

--- gulpfile.js
+++ gulpfile.js
@@ -5,59 +5,47 @@ var gulp    = require('gulp');
 var clean   = require('gulp-clean');
 var concat  = require('gulp-concat');
 var csso    = require('gulp-csso');
+var embedlr = require('gulp-embedlr');
+var refresh = require('gulp-livereload');
 var inject  = require("gulp-inject");
 var jshint  = require('gulp-jshint');
 var less    = require('gulp-less');
 var rename  = require('gulp-rename');
 var uglify  = require('gulp-uglify');
-var es      = require('event-stream');
-var stylish  = require('jshint-stylish');
-
 var gutil   = require('gulp-util');
-var embedlr = require('gulp-embedlr');
-var refresh = require('gulp-livereload');
+var es      = require('event-stream');
 var express = require('express');
 var http    = require('http');
+var stylish = require('jshint-stylish');
 var lr      = require('tiny-lr')();
 
 gulp.task('clean', function () {
     // Clear the destination folder
-    gulp.src('dist/**/*.*', { read: false })
+    return gulp.src('dist/**/*.*', { read: false })
         .pipe(clean({ force: true }));
 });
 
 gulp.task('copy', function () {
     // Copy all application files except *.less and .js into the `dist` folder
-    return es.concat(
-        gulp.src(['src/material/**'])
-            .pipe(gulp.dest('dist/material')),
-        gulp.src(['src/js/vendor/**'])
-            .pipe(gulp.dest('dist/js/vendor')),
-        gulp.src(['src/css/assets/**'])
-            .pipe(gulp.dest('dist/css/assets')),
-        gulp.src(['src/css/fonts/**'])
-            .pipe(gulp.dest('dist/css/fonts')),
-        gulp.src(['src/css/sprites/**'])
-            .pipe(gulp.dest('dist/css/sprites')),
-        gulp.src(['src/*.*'])
-            .pipe(gulp.dest('dist'))
-    );
+    return gulp.src(['src/**/*', '!src/js/*.js', '!src/less/**/*.less'])
+            .pipe(gulp.dest('dist'));
 });
 
-gulp.task('scripts', function () {
-    return es.concat(
-        // Detect errors and potential problems in your JavaScript code
-        // You can enable or disable default JSHint options in the .jshintrc file
-        gulp.src(['src/js/**/*.js', '!src/js/vendor/**'])
+gulp.task('jshint', function () {
+    // Detect errors and potential problems in your JavaScript code
+    // You can enable or disable default JSHint options in the .jshintrc file
+    return gulp.src(['src/js/**/*.js', '!src/js/vendor/**'])
             .pipe(jshint('.jshintrc'))
-            .pipe(jshint.reporter(stylish)),
+            .pipe(jshint.reporter(stylish));
+});
 
-        // Concatenate, minify and copy all JavaScript (except vendor scripts)
-        gulp.src(['src/js/**/*.js', '!src/js/vendor/**'])
+
+gulp.task('scripts', function () {
+    // Concatenate, minify and copy all JavaScript (except vendor scripts)
+    return gulp.src(['src/js/**/*.js', '!src/js/vendor/**'])
             .pipe(concat('app.js'))
             .pipe(uglify())
-            .pipe(gulp.dest('dist/js'))
-    );
+            .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('styles', function () {
@@ -69,7 +57,7 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('dist/css'))
 });
 
-gulp.task('html', function() {
+gulp.task('html', ['styles', 'scripts'] , function() {
     // We src all html files
     return gulp.src('src/*.html')
         .pipe(inject(gulp.src(["./dist/**/*.*", '!./dist/js/vendor/**'], {read: false}), {
@@ -83,6 +71,7 @@ gulp.task('server', function () {
     // Create a HTTP server for static files
     var port = 3000;
     var app = express();
+
     var server = http.createServer(app);
 
     app.use(express.static(__dirname + '/dist'));
@@ -129,4 +118,4 @@ gulp.task('watch', function () {
 // The default task (called when you run `gulp`)
 gulp.task('local', ['clean', 'copy', 'scripts', 'styles', 'html', 'lr-server', 'server', 'watch']);
 
-gulp.task('default', ['clean', 'copy', 'scripts', 'styles', 'html']);
+gulp.task('default', ['clean', 'copy', 'jshint', 'scripts', 'styles', 'html']);
