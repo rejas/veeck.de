9aa84f09941f1ec6c2b3064f4d1fca4131b3be68
Veeck <veeck@neofonie.de>
2014-04-01 15:39:16 +0200

    cop y.dot files and clean up dist dir for good

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 // https://github.com/gulpjs/gulp
 
 // TODO
-// - fix .htaccess copy
 // - randomize name of app.css/js on deploy
 // - test and surely fix livereload stuff
 
@@ -26,13 +25,15 @@ var lr      = require('tiny-lr')();
 
 gulp.task('clean', function () {
     // Clear the destination folder
-    return gulp.src('dist/**/*.*', { read: false })
+    return gulp.src('dist/', { read: false })
         .pipe(clean({ force: true }));
 });
 
 gulp.task('copy', function () {
     // Copy all application files except *.less and .js into the `dist` folder
-    return gulp.src(['src/**/*', '!src/js/**/*.js', '!src/less/**/*.less'])
+    return gulp.src(['src/**/*', '!src/js/**/*.js', '!src/less/**/*.less'], {
+        dot: true
+    })
             .pipe(gulp.dest('dist'));
 });
 
