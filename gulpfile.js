be4995189fb0910e43663318738a166ac503860e
rejas <github@veeck.de>
2014-06-15 22:36:24 +0200

    use gulp-plugin loading

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,11 @@
 // TODO
 // - randomize name of app.css/js on deploy
 
-var gulp    = require('gulp');
-var clean   = require('gulp-clean');
-var concat  = require('gulp-concat');
-var csslint = require('gulp-csslint');
-var csso    = require('gulp-csso');
-var ftp     = require('gulp-ftp');
-var imagemin    = require('gulp-imagemin');
-var inject      = require("gulp-inject");
-var jshint      = require('gulp-jshint');
-var less        = require('gulp-less');
-var livereload  = require('gulp-livereload');
-var prompt  = require('gulp-prompt');
-var rename  = require('gulp-rename');
-var sitemap = require('gulp-sitemap');
-var uglify  = require('gulp-uglify');
-var uncss   = require('gulp-uncss');
 var gutil   = require('gulp-util');
+var gulp            = require('gulp'),
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    plugins         = gulpLoadPlugins();
+
 var express = require('express');
 var stylish = require('jshint-stylish');
 var tiny    = require('tiny-lr');
@@ -35,7 +23,7 @@ var EXPRESS_ROOT    = __dirname + '/' + SRC;
 // Clear the destination folder
 gulp.task('clean', function () {
     return gulp.src(DST, { read: false })
-        .pipe(clean({ force: true }));
+        .pipe(plugins.clean({ force: true }));
 });
 
 gulp.task('copy', ['clean'], function () {
@@ -46,53 +34,53 @@ gulp.task('copy', ['clean'], function () {
 
 gulp.task('images', function () {
     gulp.src('src/material/img/**/*')
-        .pipe(imagemin())
+        .pipe(plugins.imagemin())
         .pipe(gulp.dest('dist/material/img'));
 });
 
 // Detect errors and potential problems in your css code
 gulp.task('csslint', function () {
     return gulp.src([SRC + 'css/*.less', '!'+SRC +'css/normalize.less'])
-        .pipe(csslint('.csslintrc'))
-        .pipe(csslint.reporter())
+        .pipe(plugins.csslint('.csslintrc'))
+        .pipe(plugins.csslint.reporter())
 });
 
 // Detect errors and potential problems in your JavaScript code (except vendor scripts)
 // You can enable or disable default JSHint options in the .jshintrc file
 gulp.task('jshint', function () {
     return gulp.src([SRC + 'js/**/*.js', '!'+SRC +'js/vendor/**'])
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter(stylish));
+        .pipe(plugins.jshint('.jshintrc'))
+        .pipe(plugins.jshint.reporter(stylish));
 });
 
 gulp.task('vendorscripts', ['clean'], function () {
     // Minify and copy all vendor scripts
     return gulp.src(['src/js/vendor/**'])
-        .pipe(uglify())
+        .pipe(plugins.uglify())
         .pipe(gulp.dest('dist/js/vendor'));
 });
 
 gulp.task('scripts', ['clean'], function () {
     // Concatenate, minify and copy all JavaScript (except vendor scripts)
     return gulp.src(['src/js/**/*.js', '!src/js/vendor/**'])
-        .pipe(concat('app.js'))
-        .pipe(uglify())
+        .pipe(plugins.concat('app.js'))
+        .pipe(plugins.uglify())
         .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('styles', ['clean'], function () {
     // Compile LESS files
     return gulp.src('src/css/main.less')
-        .pipe(less())
-        .pipe(rename('app.css'))
-        .pipe(csso())
+        .pipe(plugins.less())
+        .pipe(plugins.rename('app.css'))
+        .pipe(plugins.csso())
         .pipe(gulp.dest('dist/css'))
 });
 
 gulp.task('html', ['styles', 'scripts'] , function() {
     // We src all html files
     return gulp.src('src/*.html')
-        .pipe(inject(gulp.src(["./dist/**/*.*", '!./dist/js/vendor/**'], {read: false}), {
+        .pipe(plugins.inject(gulp.src(["./dist/**/*.*", '!./dist/js/vendor/**'], {read: false}), {
             addRootSlash: false,  // ensures proper relative paths
             ignorePath: '/dist/' // ensures proper relative paths
         }))
@@ -102,16 +90,16 @@ gulp.task('html', ['styles', 'scripts'] , function() {
 gulp.task('uncss', ['html'], function() {
     // Optimize via Uncss (beware: doesnt work with JS styles like in mobilemenu)
     return gulp.src('dist/css/app.css')
-        .pipe(uncss({
+        .pipe(plugins.uncss({
             html: ['dist/index.html']
         }))
-        .pipe(csso())
+        .pipe(plugins.csso())
         .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('sitemap', ['html'], function () {
     return gulp.src(['src/**/*.html', '!src/**/google*.html'], {read: false})
-        .pipe(sitemap({
+        .pipe(plugins.sitemap({
             fileName: 'sitemap.xml',
             newLine: '\n',
             changeFreq: 'daily',
@@ -140,19 +128,19 @@ gulp.task('serve', function () {
 
     gulp.watch([SRC + '**/*.html', SRC + 'css/**/*.less', SRC + 'js/**/*.js'] , function (event) {
         gulp.src(event.path, {read: false})
-            .pipe(livereload(lr));
+            .pipe(plugins.livereload(lr));
     });
 });
 
 gulp.task('ftp', function () {
     return gulp.src('.')
-        .pipe(prompt.prompt({
+        .pipe(plugins.prompt.prompt({
             type: 'password',
             name: 'pw',
             message: 'enter ftp password'
         }, function(result){
             return gulp.src('dist/**/*')
-                .pipe(ftp({
+                .pipe(plugins.ftp({
                     host: 'www.veeck.de',
                     user: 'www.veeck.de' ,
                     remotePath: 'dist',
