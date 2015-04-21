296c9ead1c0f0baa2858920e4dbb05139730dc5d
rejas <github@veeck.de>
2015-04-21 20:39:58 +0200

    move more stuff into config file7

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,6 @@ var gulp            = require('gulp'),
 var connect         = require('connect-livereload'),
     del             = require('del'),
     express         = require('express'),
-    pngquant        = require('imagemin-pngquant'),
     stylish         = require('jshint-stylish'),
     tiny            = require('tiny-lr');
 
@@ -26,39 +25,30 @@ var connect         = require('connect-livereload'),
 var config          = require('./config.json'),
     dirs            = config.directories;
 
-var SRC             = 'src/';
-var DST             = 'dist/';
-var LIVERELOAD_PORT = 35729;
-var EXPRESS_PORT    = 4000;
-var EXPRESS_ROOT    = __dirname + '/' + SRC;
-
 /**
  * SUB TASKS
  */
 
 // Clear the destination folder
 gulp.task('clean', function (cb) {
-    del([DST], cb)
+    del([dirs.dist], cb)
 });
 
 // Copy all application files except *.less and .js into the `dist` folder
 gulp.task('copy', ['clean'], function () {
     return gulp.src(['src/**/*', '!src/js/**/*.js', '!src/css/**/*.less'], { dot: true })
-        .pipe(gulp.dest(DST));
+        .pipe(gulp.dest(dirs.dist));
 });
 
 gulp.task('images', ['copy'], function () {
-    gulp.src(SRC + 'material/img/**/*.{jpg|png}')
-        .pipe(plugins.imagemin({
-            progressive: true,
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest(DST+'material/img'));
+    gulp.src(dirs.src + 'material/img/**/*.{jpg|png}')
+        .pipe(plugins.imagemin(config.imagemin))
+        .pipe(gulp.dest(dirs.dist+'material/img'));
 });
 
 // Detect errors and potential problems in your css code
 gulp.task('csslint', function () {
-    return gulp.src([SRC + 'css/*.less', '!'+SRC +'css/normalize.less'])
+    return gulp.src([dirs.src + 'css/*.less', '!'+dirs.src +'css/normalize.less'])
         .pipe(plugins.csslint('.csslintrc'))
         .pipe(plugins.csslint.reporter())
 });
@@ -66,14 +56,14 @@ gulp.task('csslint', function () {
 // Detect errors and potential problems in your JavaScript code (except vendor scripts)
 // You can enable or disable default JSHint options in the .jshintrc file
 gulp.task('jshint', function () {
-    return gulp.src([SRC + 'js/**/*.js', '!'+SRC +'js/vendor/**'])
+    return gulp.src([dirs.src + 'js/**/*.js', '!'+dirs.src +'js/vendor/**'])
         .pipe(plugins.jshint('.jshintrc'))
         .pipe(plugins.jshint.reporter(stylish));
 });
 
 // Detect errors and potential problems in your html code
 gulp.task('htmlhint', function () {
-    return gulp.src([SRC + '*.html'])
+    return gulp.src([dirs.src + '*.html'])
         .pipe(plugins.htmlhint())
         .pipe(plugins.htmlhint.reporter());
 });
@@ -82,7 +72,7 @@ gulp.task('vendorscripts', ['clean'], function () {
     // Minify and copy all vendor scripts
     return gulp.src(['src/js/vendor/**'])
         .pipe(plugins.uglify())
-        .pipe(gulp.dest(DST+'js/vendor'));
+        .pipe(gulp.dest(dirs.dist+'js/vendor'));
 });
 
 // Concatenate, minify and copy all JavaScript (except vendor scripts)
@@ -91,7 +81,7 @@ gulp.task('scripts', ['clean'], function () {
         .pipe(plugins.concat('app.js'))
         .pipe(plugins.rev())
         .pipe(plugins.uglify())
-        .pipe(gulp.dest(DST+'js'));
+        .pipe(gulp.dest(dirs.dist+'js'));
 });
 
 // Compile LESS files
@@ -102,28 +92,25 @@ gulp.task('styles', ['clean'], function () {
         .pipe(plugins.rename('app.css'))
         .pipe(plugins.rev())
         .pipe(plugins.csso())
-        .pipe(gulp.dest(DST+'css'))
+        .pipe(gulp.dest(dirs.dist+'css'))
 });
 
 gulp.task('html', ['images', 'styles', 'scripts', 'vendorscripts'] , function() {
     // We src all html files
     return gulp.src('src/*.html')
-        .pipe(plugins.inject(gulp.src(["./dist/**/*.*", '!./dist/js/vendor/**'], {read: false}), {
-            addRootSlash: false,  // ensures proper relative paths
-            ignorePath: '/dist/' // ensures proper relative paths
-        }))
+        .pipe(plugins.inject(gulp.src(["./dist/**/*.*", '!./dist/js/vendor/**'], {read: false}), config.inject))
         .pipe(minifyHTML(config.minifyHTML))
-        .pipe(gulp.dest(DST));
+        .pipe(gulp.dest(dirs.dist));
 });
 
 // Optimize via Uncss (beware: doesnt work with JS styles like in mobilemenu)
 gulp.task('uncss', ['html'], function() {
-    return gulp.src(DST+'css/app.css')
+    return gulp.src(dirs.dist+'css/app.css')
         .pipe(uncss({
-            html: [DST+'index.html']
+            html: [dirs.dist+'index.html']
         }))
         .pipe(plugins.csso())
-        .pipe(gulp.dest(DST+'css/'));
+        .pipe(gulp.dest(dirs.dist+'css/'));
 });
 
 gulp.task('sitemap', ['html'], function () {
@@ -141,21 +128,24 @@ gulp.task('sitemap', ['html'], function () {
 
 // Start express- and live-reload-server
 gulp.task('serve', function () {
-    var server = express();
+    var server  = express(),
+        ports    = config.ports,
+        root    = __dirname + '/' + dirs.src;
+
     server.use(connect());
-    server.use(express.static(EXPRESS_ROOT));
-    server.listen(EXPRESS_PORT, function() {
-        gutil.log('Listening on port ' + EXPRESS_PORT);
+    server.use(express.static(root));
+    server.listen(ports.express, function() {
+        gutil.log('Listening on port ' + ports.express);
     });
 
     var lr = tiny();
-    lr.listen(LIVERELOAD_PORT, function (err) {
+    lr.listen(ports.livereload, function (err) {
         if (err) {
             gutil.log(err);
         }
     });
 
-    gulp.watch([SRC + '**/*.html', SRC + 'css/**/*.less', SRC + 'js/**/*.js'] , function (event) {
+    gulp.watch([dirs.src + '**/*.html', dirs.src + 'css/**/*.less', dirs.src + 'js/**/*.js'] , function (event) {
         gulp.src(event.path, {read: false})
             .pipe(plugins.livereload(lr));
     });
@@ -168,7 +158,7 @@ gulp.task('ftp', function () {
             name: 'pw',
             message: 'enter ftp password'
         }, function(result){
-            return gulp.src(DST+'**/*')
+            return gulp.src(dirs.dist+'**/*')
                 .pipe(plugins.ftp({
                     host: 'www.veeck.de',
                     user: 'www.veeck.de' ,
