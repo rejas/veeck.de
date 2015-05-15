a90c3a2d2e2bdef188b40181102eb6995e03a59d
rejas <github@veeck.de>
2015-05-15 22:41:54 +0200

    add animation to frontpagelinks

--- src/js/main.js
+++ src/js/main.js
@@ -39,7 +39,38 @@ $(document).ready(function ()
         zindex: 10
     });
 
+    /**
+     * Fill out the background header images
+     */
     $(".imgLiquidFill").imgLiquid({useBackgroundSize: true});
+
+    /**
+     * Animsitions
+     */
+    $(".animsition").animsition({
+
+        inClass               :   'zoom-in-sm',
+        outClass              :   'zoom-out-sm',
+        inDuration            :    1500,
+        outDuration           :    800,
+        linkElement           :   '.animsition-link',
+        // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
+        loading               :    true,
+        loadingParentElement  :   'body', //animsition wrapper element
+        loadingClass          :   'animsition-loading',
+        unSupportCss          : [ 'animation-duration',
+                                  '-webkit-animation-duration',
+                                  '-o-animation-duration'
+        ],
+        //"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+        //The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+
+        overlay               :   false,
+
+        overlayClass          :   'animsition-overlay-slide',
+        overlayParentElement  :   'body'
+    });
+
 
     /**
      * ImageLightBox
