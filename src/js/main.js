a4151b37d87c6eb6bc55285aa8ee842c3fdf9323
rejas <github@veeck.de>
2016-04-10 17:26:59 +0200

    [WEB] add color variables inspired by https://justmarkup.com/log/2016/02/theme-switcher-using-css-custom-properties/

--- src/js/main.js
+++ src/js/main.js
@@ -82,6 +82,48 @@ $(document).ready(function() {
     };
 
     /**
+     *    CSS provides HSL color mode that controls Hue, Saturation, Luminosity(Lightness) and optionaly Opacity
+     *
+     *    color: hsla(50, 80%, 20%, 0.5);
+     *    background-color: hsl(120, 100%, 50%);
+     *
+     *    hex —> hex color value such as “#abc” or “#123456″ (the hash is optional)
+     *    lum —> luminosity factor: -0.1 is 10% darker, 0.2 is 20% lighter
+     */
+    function ColorLuminance(hex, lum) {
+        var rgb = '#', c, i;
+
+        // validate hex string
+        hex = String(hex).replace(/[^0-9a-f]/gi, '');
+        if(hex.length < 6) {
+            hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+        }
+        lum = lum || 0;
+
+        // convert to decimal and change luminosity
+        for(i = 0; i < 3; i++) {
+            c = parseInt(hex.substr(i * 2, 2), 16);
+            c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+            rgb += ('00' + c).substr(c.length);
+        }
+
+        return rgb;
+    }
+
+    /**
+     *
+     */
+    if (window.CSS && window.CSS.supports && window.CSS.supports('--primaryColor', 0)) {
+        // CSS custom properties supported.
+        var root = document.querySelector(':root');
+        var htmlStyle = window.getComputedStyle(root);
+        htmlStyle.getPropertyValue('--primaryColor');
+        root.style.setProperty('--primaryColor', '#3949AB');
+        root.style.setProperty('--lightPrimaryColor', ColorLuminance("3949AB", 0.35));
+        root.style.setProperty('--darkPrimaryColor', ColorLuminance("3949AB", -0.35));
+    }
+
+    /**
      * Back to top
      */
     $('.js-to-top').on('click', function(e) {
