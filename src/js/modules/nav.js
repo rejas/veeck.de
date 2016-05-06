6099257f3bc3ad6801ebe475a5199ff4bee04547
rejas <github@veeck.de>
2016-05-06 19:12:05 +0200

    [NAV] move into own module

---
+++ src/js/modules/nav.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var MQ = require('../../components/on-media-query/js/onmediaquery.js'),
+    $trigger;
+
+function cssButton() {
+
+    // trigger mobile button animation
+    $trigger = $('.js-dl-trigger');
+
+    $trigger.on('click', function(ignore) {
+
+        $trigger.toggleClass('is-active');
+
+        if ($trigger.hasClass('is-active')) {
+
+            $('body').off('click').children().on('click', function() {
+                $trigger.removeClass('is-active');
+            });
+        }
+    });
+}
+
+module.exports = {
+
+    init: function() {
+
+        /**
+         * decide if mobile or not
+         * @type {Array}
+         */
+        var $myNav = $('nav').clone(),
+            queries = [
+            {
+                context: 'mobile',
+                match: function() {
+                    $('nav').remove();
+                    $myNav.clone().prependTo('header').addClass('mobile-nav').removeClass('desktop-nav').dlmenu();
+                    cssButton();
+                },
+                unmatch: function() {
+                }
+            },
+            {
+                context: 'desktop',
+                match: function() {
+                    $('nav').remove();
+                    $myNav.clone().prependTo('header').removeClass('mobile-nav').addClass('desktop-nav');
+                },
+                unmatch: function() {
+                }
+            }
+        ];
+        MQ.init(queries);
+    }
+};
