14cfce0f186f74007839d9c1adc978b645c4d2b6
Veeck, Michael(AWF) <mveeck@team.mobile.de>
2017-02-07 11:07:25 +0100

    Add mqe-code

--- src/js/modules/mqe.js
+++ src/js/modules/mqe.js
@@ -1,10 +1,36 @@
-/**
- *
- * @type {{init: module.exports.init}}
- */
+
 module.exports = {
 
-    init: function (options) {
+    init: function (mqs) {
 
+        var handleMediaChange = function (mediaQueryList) {
+            var media = mqs.filter(function (el) {
+                return el.media === mediaQueryList.media;
+            });
+
+            if (media.length === 0) {
+                return;
+            }
+            media = media[0];
+
+            if (mediaQueryList.matches) {
+                $("body").trigger({
+                    type: "mediaQuery:active",
+                    media: media.name
+                });
+            } else {
+                $("body").trigger({
+                    type: "mediaQuery:inactive",
+                    media: media.name
+                });
+            }
+        };
+
+        var arrayLength = mqs.length;
+        for (var i = 0; i < arrayLength; i++) {
+            var mql = window.matchMedia(mqs[i].media);
+            mql.addListener(handleMediaChange);
+            handleMediaChange(mql);
+        }
     }
 };
