e7c28113ec7b9ffbbff54fa303c642131657bd45
rejas <github@veeck.de>
2017-03-24 18:52:09 +0100

    Use png instead of font for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,6 +132,7 @@ module.exports = function(grunt) {
                 processors: [
                     require('autoprefixer')(config.autoprefixer),
                     require('css-mqpacker')(),
+                    require('cssnano'),
                     require('postcss-sprites')({
                         spritePath: './dist/styles/'
                     })
