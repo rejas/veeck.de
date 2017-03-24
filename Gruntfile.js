03e66682cd6b7eb00bc66b7cddb8e5bb7904d81e
rejas <github@veeck.de>
2017-03-24 15:35:24 +0100

    Use postcss for sprite generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,12 +51,14 @@ module.exports = function(grunt) {
         },
 
         copy: {
+            /*
             assets: {
                 expand: true,
                 cwd: '<%= dir.src %>/assets/',
                 src: '**',
                 dest: '<%= dir.dist %>/assets/'
             },
+            */
             files: {
                 expand: true,
                 cwd: '<%= dir.src %>/files/',
@@ -101,7 +103,7 @@ module.exports = function(grunt) {
                     paths: ['<%= dir.src %>/css']
                 },
                 files: {
-                    '<%= dir.dist %>/assets/main.css': '<%= dir.src %>/css/main.less'
+                    '<%= dir.dist %>/styles/main.css': '<%= dir.src %>/css/main.less'
                 }
             }
         },
@@ -130,11 +132,13 @@ module.exports = function(grunt) {
                 processors: [
                     require('autoprefixer')(config.autoprefixer),
                     require('css-mqpacker')(),
-                    require('cssnano')()
+                    require('postcss-sprites')({
+                        spritePath: './dist/styles/'
+                    })
                 ]
             },
             dist: {
-                src: '<%= dir.dist %>/assets/main.css'
+                src: '<%= dir.dist %>/styles/main.css'
             }
         },
 
@@ -157,7 +161,7 @@ module.exports = function(grunt) {
                 },
                 files: [
                     '<%= dir.dist %>/*.html',
-                    '<%= dir.dist %>/assets/**/*.css',
+                    '<%= dir.dist %>/styles/**/*.css',
                     '<%= dir.dist %>/js/**/*.js',
                     '<%= dir.dist %>/img/**/*.{png,jpg,jpeg,gif,webp,svg}'
                 ]
