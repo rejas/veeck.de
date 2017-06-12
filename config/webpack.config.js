5b2c2459b7b2abc9a2665005eec7b2e1d8c36b34
Veeck <github@veeck.de>
2017-06-12 22:24:30 +0200

    Add eslint loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -15,7 +15,12 @@ const webpackconfig = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /(node_modules)/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'eslint-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
                 use: [{
                     loader: 'babel-loader',
                     options: {
