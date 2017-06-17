aa7a01596f048359e04777942f2d16717d7720d7
Veeck <github@veeck.de>
2017-06-17 12:40:32 +0200

    Copy dot files too

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,6 +64,7 @@ module.exports = function(grunt) {
                 dest: '<%= dir.dist %>/img/'
             },
             page: {
+                dot: true,
                 expand: true,
                 cwd: '<%= dir.src %>/page',
                 src: '**',
