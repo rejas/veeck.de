5f30beb8fe7bf279e12ba00cfd50061663d0d846
Veeck <veeck@neofonie.de>
2017-07-21 19:21:26 +0200

    Add jimp, remove grunt

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,4 @@
-const config = require('./grunt.config.js'),
+const config = require('./gulp.config.js'),
     path = require('path'),
     webpack = require('webpack'),
     ExtractTextPlugin = require('extract-text-webpack-plugin'),
