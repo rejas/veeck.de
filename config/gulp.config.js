75ec22ac8069e269d96294092ff388f2677a220c
Veeck <github@veeck.de>
2017-07-21 23:04:36 +0200

    Update modernizr

--- config/gulp.config.js
+++ config/gulp.config.js
@@ -6,6 +6,7 @@ const config = {
     },
     directories: {
         assemble: "./src/assemble",
+        node: "./node_modules",
         org: "./org",
         src: "./src",
         tmp: "./tmp",
