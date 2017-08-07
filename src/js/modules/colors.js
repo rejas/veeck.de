23ef44ca275ce748954b1298803d627136143161
Veeck <github@veeck.de>
2017-08-07 23:58:08 +0200

    Big cleanup of color schemes

--- src/js/modules/colors.js
+++ src/js/modules/colors.js
@@ -4,22 +4,29 @@ import tinycolor from 'tinycolor2';
 function changeColor (c) {
     const root = document.querySelector(':root'),
         htmlStyle = window.getComputedStyle(root),
-        color = tinycolor(c);
+        color = tinycolor(c),
+        stc = htmlStyle.getPropertyValue('--secondaryTextColor');
 
-    htmlStyle.getPropertyValue('--primaryColor');
-    root.style.setProperty('--primaryColor', color.toHexString());
-    root.style.setProperty('--darkPrimaryColor', color.darken(15));
-    root.style.setProperty('--lightPrimaryColor', color.lighten(30));
-    root.style.setProperty('--accentColor', color.lighten(15));
-
-    if (color.getBrightness() < 200) {
-        root.style.setProperty('--secondaryTextColor', '#fcfcfc');
+    if (color.isLight()) {
+        root.style.setProperty('--primaryTextColor', stc);
+        //TODO adjust secondaryTextColor accordingly
     }
+
+    root.style.setProperty('--primaryColor', color.toHexString());
+    root.style.setProperty('--darkPrimaryColor', color.darken(30).toHexString());
+    root.style.setProperty('--lightPrimaryColor', color.lighten(45).toHexString());
+    //TODO make accent color calculation same as in less code
+    root.style.setProperty('--accentColor', color.darken(15).complement().toHexString());
+    root.style.setProperty('--darkAccentColor', color.darken(30).toHexString());
+    root.style.setProperty('--lightAccentColor', color.lighten(45).toHexString());
 }
 
 export function init() {
     let newColor = document.querySelector('header').getAttribute('data-color');
 
+    //TODO add random use of header or random color
+    //TODO dont generate random color that is too bright or too dark
+    //MAYBE tinycolor has a rng method?
     if (!newColor)
         newColor = '000000'.replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
 
