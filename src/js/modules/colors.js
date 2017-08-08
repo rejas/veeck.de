edd53c2f6d9853fcea4d1da5111305d3d5537837
Veeck <veeck@neofonie.de>
2017-08-08 13:19:08 +0200

    More color cleanups

--- src/js/modules/colors.js
+++ src/js/modules/colors.js
@@ -4,19 +4,20 @@ import tinycolor from 'tinycolor2';
 function changeColor (c) {
     const root = document.querySelector(':root'),
         htmlStyle = window.getComputedStyle(root),
-        color = tinycolor(c),
-        stc = htmlStyle.getPropertyValue('--secondaryTextColor');
-
-    if (color.isLight()) {
-        root.style.setProperty('--primaryTextColor', stc);
-        //TODO adjust secondaryTextColor accordingly
-    }
+        ptc = htmlStyle.getPropertyValue('--primaryTextColor'),
+        stc = htmlStyle.getPropertyValue('--secondaryTextColor'),
+        color = tinycolor(c);
 
     root.style.setProperty('--primaryColor', color.toHexString());
+    root.style.setProperty('--primaryTextColor', tinycolor.mostReadable(color, [ptc, stc]).toHexString());
+
     root.style.setProperty('--darkPrimaryColor', color.darken(30).toHexString());
     root.style.setProperty('--lightPrimaryColor', color.lighten(45).toHexString());
-    root.style.setProperty('--accentColor', color.darken(15).splitcomplement()[1].toHexString());
-    root.style.setProperty('--secondaryAccentColor', color.splitcomplement()[2].toHexString());
+
+    var splitColor = color.darken(15).splitcomplement();
+    root.style.setProperty('--accentColor', splitColor[1].toHexString());
+    root.style.setProperty('--secondaryAccentColor', splitColor[2].toHexString());
+    root.style.setProperty('--secondaryTextColor', tinycolor.mostReadable(splitColor[1], [ptc, stc]).toHexString());
 }
 
 export function init() {
