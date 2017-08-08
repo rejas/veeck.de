894fe33b33d5f01606e4edca4d3170a46b631268
Veeck <github@veeck.de>
2017-08-08 19:37:28 +0200

    Use random color and tone it down a noth

--- src/js/modules/colors.js
+++ src/js/modules/colors.js
@@ -1,20 +1,19 @@
 /* global Modernizr */
 import tinycolor from 'tinycolor2';
 
-function changeColor (c) {
+function changeColor (color) {
     const root = document.querySelector(':root'),
         htmlStyle = window.getComputedStyle(root),
         ptc = htmlStyle.getPropertyValue('--primaryTextColor'),
         stc = htmlStyle.getPropertyValue('--secondaryTextColor'),
-        color = tinycolor(c);
+        splitColor = color.splitcomplement();
 
     root.style.setProperty('--primaryColor', color.toHexString());
     root.style.setProperty('--primaryTextColor', tinycolor.mostReadable(color, [ptc, stc]).toHexString());
 
-    root.style.setProperty('--darkPrimaryColor', color.darken(30).toHexString());
-    root.style.setProperty('--lightPrimaryColor', color.lighten(45).toHexString());
+    root.style.setProperty('--darkPrimaryColor', color.clone().darken(10).toHexString());
+    root.style.setProperty('--lightPrimaryColor', color.clone().lighten(20).toHexString());
 
-    var splitColor = color.darken(15).splitcomplement();
     root.style.setProperty('--accentColor', splitColor[1].toHexString());
     root.style.setProperty('--secondaryAccentColor', splitColor[2].toHexString());
     root.style.setProperty('--secondaryTextColor', tinycolor.mostReadable(splitColor[1], [ptc, stc]).toHexString());
@@ -24,12 +23,13 @@ export function init() {
     let newColor = document.querySelector('header').getAttribute('data-color');
 
     //TODO add random use of header or random color
-    //TODO dont generate random color that is too bright or too dark
-    //MAYBE tinycolor has a rng method?
-    if (!newColor)
-        newColor = '000000'.replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+    if (!newColor) {
+        newColor = tinycolor.random().toHsl();
+        if (newColor.l < 0.25) newColor.l = 0.25;
+        if (newColor.l > 0.75) newColor.l = 0.75;
+    }
 
     if (Modernizr.supports && window.CSS.supports('--primaryColor', 0)) {
-        changeColor(newColor);
+        changeColor(tinycolor(newColor));
     }
 }
