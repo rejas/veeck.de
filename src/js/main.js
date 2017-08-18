159fa0b8699c5b7ee84721608fa2775c298dfda5
Veeck <veeck@neofonie.de>
2017-08-18 16:43:09 +0200

    Remove old console helper function

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,6 @@
 /* global Modernizr */
 'use strict';
 
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function() {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
-}());
-
 // Event listener: DOM ready
 function addLoadEvent(func) {
     var oldonload = window.onload;
