9bede5cf874f8606e8ab03ad55b3c3e238704624
Veeck <github@veeck.de>
2017-08-20 21:42:17 +0200

    More cleanups

--- src/js/main.js
+++ src/js/main.js
@@ -32,11 +32,7 @@ addLoadEvent(function() {
     });
 });
 
-import styles_webpack from '../css/main.less';
-
-import 'cookieconsent/src/cookieconsent';
-import 'imagelightbox';
-import 'responsivemultilevelmenu/js/jquery.dlmenu';
+import styles_webpack   from '../css/main.less';
 
 import * as Colors      from './modules/colors';
 import * as Input       from './modules/input';
@@ -48,6 +44,10 @@ import BrowserUpdate    from 'browser-update';
 import Konami           from 'konami-code.js';
 import objectFitImages  from 'object-fit-images';
 import Smoothscroll     from 'smoothscroll-polyfill';
+
+import 'cookieconsent/src/cookieconsent';
+import 'imagelightbox';
+import 'responsivemultilevelmenu/js/jquery.dlmenu';
 
 document.addEventListener('DOMContentLoaded', () => {
 
