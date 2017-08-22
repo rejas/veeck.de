dfaa2b140611cd7b4b475b1ab5e4b08d02ad9df6
Veeck <github@veeck.de>
2017-08-22 08:44:54 +0200

    Use actual nav data

--- src/js/modules/nav.js
+++ src/js/modules/nav.js
@@ -1,17 +1,15 @@
 import 'MultiLevelMenu/js/main.js';
 
 export function init() {
-    var menuEl = document.getElementById('ml-menu'),
+    let menuEl = document.getElementById('ml-menu'),
         mlmenu = new MLMenu(menuEl, {
             // breadcrumbsCtrl : true, // show breadcrumbs
             // initialBreadcrumb : 'all', // initial breadcrumb text
             backCtrl : false, // show back button
             // itemsDelayInterval : 60, // delay between each menu item sliding animation
             // onItemClick: loadDummyData // callback: item that doesn´t have a submenu gets clicked - onItemClick([event], [inner HTML of the clicked item])
-        });
-
-    // mobile menu toggle
-    var openMenuCtrl = document.querySelector('.action--open'),
+        }),
+        openMenuCtrl = document.querySelector('.action--open'),
         closeMenuCtrl = document.querySelector('.action--close');
 
     openMenuCtrl.addEventListener('click', openMenu);
