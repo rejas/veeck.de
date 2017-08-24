89ee85b7295bb9e86e22f46355680c7f1d1ad2cb
Veeck <veeck@neofonie.de>
2017-08-24 11:39:23 +0200

    Small nav color updates

--- src/js/modules/nav.js
+++ src/js/modules/nav.js
@@ -3,11 +3,11 @@ import 'MultiLevelMenu/js/main.js';
 export function init() {
     let menuEl = document.getElementById('ml-menu'),
         mlmenu = new MLMenu(menuEl, {
-            // breadcrumbsCtrl : true, // show breadcrumbs
-            // initialBreadcrumb : 'all', // initial breadcrumb text
-            backCtrl : false, // show back button
-            // itemsDelayInterval : 60, // delay between each menu item sliding animation
-            // onItemClick: loadDummyData // callback: item that doesn´t have a submenu gets clicked - onItemClick([event], [inner HTML of the clicked item])
+            breadcrumbsCtrl: true,         // show breadcrumbs
+            initialBreadcrumb: 'menu',    // initial breadcrumb text
+            // itemsDelayInterval: 60,      // delay between each menu item sliding animation
+            // onItemClick: loadDummyData,  // callback: item that doesn´t have a submenu gets clicked - onItemClick([event], [inner HTML of the clicked item])
+            backCtrl: false                 // show back button
         }),
         openMenuCtrl = document.querySelector('.action--open'),
         closeMenuCtrl = document.querySelector('.action--close');
