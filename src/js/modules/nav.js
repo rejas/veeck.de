eb2a40648a9ecc5ce132b4f6dc91bb6e7deb6072
Veeck <veeck@neofonie.de>
2017-09-22 17:52:11 +0200

    Cleanup nav code

--- src/js/modules/nav.js
+++ src/js/modules/nav.js
@@ -1,17 +1,19 @@
-import 'MultiLevelMenu/js/main.js';
+/* global MLMenu */
+import 'multilevelmenu/js/main.js';
 
 export function init() {
     let navEl = document.querySelector('.navigation'),
         menuEl = document.getElementById('ml-menu'),
-        mlmenu = new MLMenu(menuEl, {
-            breadcrumbsCtrl: true,          // show breadcrumbs
-            initialBreadcrumb: 'menu',      // initial breadcrumb text
-            // itemsDelayInterval: 60,      // delay between each menu item sliding animation
-            // onItemClick: loadDummyData,  // callback: item that doesn´t have a submenu gets clicked - onItemClick([event], [inner HTML of the clicked item])
-            backCtrl: false                 // show back button
-        }),
         openMenuCtrl = document.querySelector('.action--open'),
         closeMenuCtrl = document.querySelector('.action--close');
+
+    new MLMenu(menuEl, {
+        breadcrumbsCtrl: true,          // show breadcrumbs
+        initialBreadcrumb: 'menu',      // initial breadcrumb text
+        // itemsDelayInterval: 60,      // delay between each menu item sliding animation
+        // onItemClick: loadDummyData,  // callback: item that doesn´t have a submenu gets clicked - onItemClick([event], [inner HTML of the clicked item])
+        backCtrl: false                 // show back button
+    });
 
     openMenuCtrl.addEventListener('click', openMenu);
     closeMenuCtrl.addEventListener('click', closeMenu);
