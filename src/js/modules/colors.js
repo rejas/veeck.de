49f8ac41dfbe9446ecac3a8c396980770b7d57e2
Veeck <github@veeck.de>
2017-10-15 00:13:59 +0200

    More color fixes

--- src/js/modules/colors.js
+++ src/js/modules/colors.js
@@ -10,8 +10,9 @@ function changeColor (color) {
     root.style.setProperty('--primaryColor', color.toRgbString());
     root.style.setProperty('--primaryTextColor', tinycolor.mostReadable(color, [ptc]).toRgbString());
 
-    root.style.setProperty('--darkPrimaryColor', color.clone().darken(7.5).toRgbString());
-    root.style.setProperty('--lightPrimaryColor', color.clone().lighten(20).toRgbString());
+    root.style.setProperty('--darkPrimaryColor', color.clone().darken(17.75).toRgbString());
+    root.style.setProperty('--lightPrimaryColor', color.clone().lighten(37.5).toRgbString());
+    root.style.setProperty('--lightPrimaryColorTransparent', color.clone().lighten(37.5).setAlpha(0.66).toRgbString());
 
     root.style.setProperty('--accentColor', accentColor.toRgbString());
     root.style.setProperty('--secondaryTextColor', tinycolor.mostReadable(accentColor, [stc]).toRgbString());
