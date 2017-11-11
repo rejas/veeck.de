3d306233c4ae7ed38461bc869119906d93377e64
Veeck <michael.veeck@gmx.net>
2017-11-11 14:58:07 +0100

    Cleanup module code

--- src/js/modules/nav.js
+++ src/js/modules/nav.js
@@ -1,38 +1,43 @@
 /* global MLMenu */
 import 'multilevelmenu/js/main.js';
 
-export function init() {
-    let navEl = document.querySelector('.navigation'),
-        menuEl = document.getElementById('ml-menu'),
-        btnEles = document.querySelectorAll('.js-btn--hamburger');
+let Nav = {
 
-    new MLMenu(menuEl, {
-        breadcrumbsCtrl: true,          // show breadcrumbs
-        initialBreadcrumb: 'menu',      // initial breadcrumb text
-        // itemsDelayInterval: 60,      // delay between each menu item sliding animation
-        // onItemClick: loadDummyData,  // callback: item that doesn´t have a submenu gets clicked - onItemClick([event], [inner HTML of the clicked item])
-        backCtrl: false                 // show back button
-    });
+    init: function() {
+        let navEl = document.querySelector('.navigation'),
+            menuEl = document.getElementById('ml-menu'),
+            btnEles = document.querySelectorAll('.js-btn--hamburger');
 
-    btnEles.forEach((element) => {
-        element.addEventListener('click', () => {
-            document.body.classList.toggle('body--locked');
-            navEl.classList.toggle('menu--open');
+        new MLMenu(menuEl, {
+            breadcrumbsCtrl: true,          // show breadcrumbs
+            initialBreadcrumb: 'menu',      // initial breadcrumb text
+            // itemsDelayInterval: 60,      // delay between each menu item sliding animation
+            // onItemClick: loadDummyData,  // callback: item that doesn´t have a submenu gets clicked - onItemClick([event], [inner HTML of the clicked item])
+            backCtrl: false                 // show back button
+        });
 
-            btnEles.forEach((element) => {
-                element.classList.toggle('is-active');
+        btnEles.forEach((element) => {
+            element.addEventListener('click', () => {
+                document.body.classList.toggle('body--locked');
+                navEl.classList.toggle('menu--open');
+
+                btnEles.forEach((element) => {
+                    element.classList.toggle('is-active');
+                });
             });
         });
-    });
 
-    document.addEventListener('click', (event) => {
-        if (!navEl.contains(event.target)) {
-            document.body.classList.remove('body--locked');
-            navEl.classList.remove('menu--open');
+        document.addEventListener('click', (event) => {
+            if (!navEl.contains(event.target)) {
+                document.body.classList.remove('body--locked');
+                navEl.classList.remove('menu--open');
 
-            btnEles.forEach((element) => {
-                element.classList.remove('is-active');
-            });
-        }
-    });
-}
+                btnEles.forEach((element) => {
+                    element.classList.remove('is-active');
+                });
+            }
+        });
+    }
+};
+
+export default Nav;
