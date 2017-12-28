24213fc6e02dbc1f3ecde7e420fa4552c10d2120
Veeck <github@veeck.de>
2017-12-28 17:48:22 +0100

    Add gulp sharp exmaple

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -363,3 +363,35 @@ gulp.task('scale',      ['scale:medium', 'scale:small', 'scale:placeholder']);
 gulp.task('upload',     ['upload:page']);
 
 grelease(gulp);
+
+var responsive = require('gulp-responsive');
+
+gulp.task('resp', function () {
+    return gulp.src('org/img/backgrounds/*.{jpg,png}')
+        .pipe(responsive({
+            '*.jpg': {
+                // Resize all JPG images to 200 pixels wide
+                width: 200,
+            },
+            '*.png': {
+                // Resize all PNG images to 50% of original pixels wide
+                width: '50%',
+            },
+            // Resize all images to 100 pixels wide and add suffix -thumbnail
+            '*': {
+                width: 100,
+                rename: { suffix: '-thumbnail' },
+            },
+        }, {
+            // Global configuration for all images
+            // The output quality for JPEG, WebP and TIFF output formats
+            quality: 70,
+            // Use progressive (interlace) scan for JPEG and PNG output
+            progressive: true,
+            // Zlib compression level of PNG output format
+            compressionLevel: 6,
+            // Strip all metadata
+            withMetadata: false,
+        }))
+        .pipe(gulp.dest('tmp2'));
+});
