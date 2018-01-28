cfad6588eb029537c00deff5f13238537d6a8ea4
Veeck <michael@veeck.de>
2018-01-28 16:12:33 +0100

    Add first new link buttons, Add logic

--- src/js/main.js
+++ src/js/main.js
@@ -118,4 +118,5 @@ document.addEventListener('DOMContentLoaded', () => {
             return false;
         });
     }
+
 });
--- src/js/modules/input.js
+++ src/js/modules/input.js
@@ -5,6 +5,9 @@
 let Input = {
 
     init: () => {
+        /**
+         * Styled Input Elements
+         */
         document.querySelectorAll('.js-input__field--manami').forEach((inputEl) => {
             // in case the input is already filled..
             if (inputEl.value.trim() !== '') {
@@ -18,6 +21,27 @@ let Input = {
                 if (event.target.value.trim() === '') {
                     event.target.parentNode.classList.remove('input--filled');
                 }
+            });
+        });
+
+        /**
+         * Links
+         */
+        document.querySelectorAll('.js-search__icon').forEach((inputEl) => {
+            inputEl.addEventListener('click', (event) => {
+                event.preventDefault();
+
+                let iform = document.querySelector('.js-links__form'),
+                    input = document.querySelector('.js-links__input'),
+                    query = document.querySelector('#search__input').value;
+
+                if (query && query !== '') {
+                    iform.action=inputEl.dataset.search;
+                    input.value = query;
+                    iform.submit();
+                }
+
+                return false;
             });
         });
     }
