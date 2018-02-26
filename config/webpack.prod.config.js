e4e9f5c7ad3505fadcb19130a0e53aec95722010
Veeck <github@veeck.de>
2018-02-26 23:35:43 +0100

    Update webpack to v4

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,20 +1,17 @@
 const baseConfig = require('./webpack.base.config.js'),
     merge = require('webpack-merge'),
-    webpack = require('webpack'),
     BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const webpackconfig = merge(baseConfig, {
-    devtool: 'source-map',
-
+    mode: 'production',
+    optimization: {
+        minimize: true
+    },
     plugins: [
         new BundleAnalyzerPlugin({
             analyzerMode: 'static',
             openAnalyzer: false,
             reportFilename: '../report.html'
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            warnings: false
         })
     ]
 });
