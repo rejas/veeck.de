3fb4535bffc4b6956fc949acc4f95bad9b9a6e1d
Veeck <github@veeck.de>
2018-02-26 23:35:43 +0100

    Update webpack to v4

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -2,7 +2,7 @@ const baseConfig = require('./webpack.base.config.js'),
     merge = require('webpack-merge');
 
 const webpackconfig = merge(baseConfig, {
-    devtool: 'eval'
+    mode: 'development'
 });
 
 module.exports = webpackconfig;
