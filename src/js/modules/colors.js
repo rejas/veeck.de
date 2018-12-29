a6c21a2bed0cf12b95d77c10eac792aa65a728c0
Veeck <github@veeck.de>
2018-12-29 11:41:02 +0100

    Cleanup figure templates

--- src/js/modules/colors.js
+++ src/js/modules/colors.js
@@ -1,30 +1,5 @@
 import tinycolor from 'tinycolor2';
 
-function changeColor (color) {
-    const root = document.querySelector(':root'),
-        htmlStyle = window.getComputedStyle(root),
-        ptc = htmlStyle.getPropertyValue('--primaryTextColor'),
-        stc = htmlStyle.getPropertyValue('--secondaryTextColor');
-
-    root.style.setProperty('--primaryColor', color.toRgbString());
-    root.style.setProperty('--primaryTextColor', tinycolor.mostReadable(color, [ptc]).toRgbString());
-
-    root.style.setProperty('--darkPrimaryColor', color.clone().darken(33.3).toRgbString());
-    root.style.setProperty('--lightPrimaryColor', color.clone().lighten(33.3).toRgbString());
-    root.style.setProperty('--lightPrimaryColorTransparent', color.clone().lighten(33.3).setAlpha(0.66).toRgbString());
-
-    const acc = color.clone().spin(180).desaturate(25);
-    root.style.setProperty('--accentColor', acc.toRgbString());
-    root.style.setProperty('--secondaryTextColor', tinycolor.mostReadable(acc, [stc]).toRgbString());
-
-    const metaThemeColor = document.querySelector('meta[name=theme-color]');
-    metaThemeColor.setAttribute('content', color.toHexString());
-
-    document.getElementById('funcR').setAttribute('tableValues', '0 ' + acc.toRgb().r / 255);
-    document.getElementById('funcG').setAttribute('tableValues', '0 ' + acc.toRgb().g / 255);
-    document.getElementById('funcB').setAttribute('tableValues', '0 ' + acc.toRgb().b / 255);
-}
-
 let Colors = {
 
     init: () => {
@@ -45,8 +20,33 @@ let Colors = {
 
             newColor.l = 0.5;
 
-            changeColor(tinycolor(newColor));
+            Colors.changeColor(tinycolor(newColor));
         }
+    },
+
+    changeColor: (color) => {
+        const root = document.querySelector(':root'),
+            htmlStyle = window.getComputedStyle(root),
+            ptc = htmlStyle.getPropertyValue('--primaryTextColor'),
+            stc = htmlStyle.getPropertyValue('--secondaryTextColor');
+
+        root.style.setProperty('--primaryColor', color.toRgbString());
+        root.style.setProperty('--primaryTextColor', tinycolor.mostReadable(color, [ptc]).toRgbString());
+
+        root.style.setProperty('--darkPrimaryColor', color.clone().darken(33.3).toRgbString());
+        root.style.setProperty('--lightPrimaryColor', color.clone().lighten(33.3).toRgbString());
+        root.style.setProperty('--lightPrimaryColorTransparent', color.clone().lighten(33.3).setAlpha(0.66).toRgbString());
+
+        const acc = color.clone().spin(180).desaturate(25);
+        root.style.setProperty('--accentColor', acc.toRgbString());
+        root.style.setProperty('--secondaryTextColor', tinycolor.mostReadable(acc, [stc]).toRgbString());
+
+        const metaThemeColor = document.querySelector('meta[name=theme-color]');
+        metaThemeColor.setAttribute('content', color.toHexString());
+
+        document.getElementById('funcR').setAttribute('tableValues', '0 ' + acc.toRgb().r / 255);
+        document.getElementById('funcG').setAttribute('tableValues', '0 ' + acc.toRgb().g / 255);
+        document.getElementById('funcB').setAttribute('tableValues', '0 ' + acc.toRgb().b / 255);
     }
 };
 
