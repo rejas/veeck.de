4477aff75c0aeeae5a46705457a986c699fe377b
Veeck <github@veeck.de>
2019-02-22 22:59:08 +0100

    Switch from uglify to terser plugin

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,13 +1,13 @@
 const baseConfig = require('./webpack.base.config.js'),
     merge = require('webpack-merge'),
     BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin,
-    UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+    TerserPlugin = require('terser-webpack-plugin');
 
 const webpackconfig = merge(baseConfig, {
     mode: 'production',
     optimization: {
         minimizer: [
-            new UglifyJsPlugin({
+            new TerserPlugin({
                 cache: true,
                 parallel: true
             })
