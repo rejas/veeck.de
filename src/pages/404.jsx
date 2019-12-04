5af16063453a518a080a46e609e8d121a286d12a
Veeck <veeck@neofonie.de>
2019-12-04 15:30:56 +0100

    Update 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
+import Typography from '@material-ui/core/Typography';
 
 import Layout from '../components/page/Layout';
 import SEO from '../components/page/Seo';
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      fileName: file(relativePath: { eq: "hal_404.jpeg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <Typography variant="h2" component="h2" gutterBottom>
+        I am sorry Dave
+      </Typography>
+      <Img fluid={data.fileName.childImageSharp.fluid} />
+      <p>I cannot let you find this page...</p>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 import Layout from '../components/page/Layout';
-import Image from '../components/Image';
-import SEO from '../components/page/Seo';
-import Typography from '@material-ui/core/Typography';
 import ProTip from '../components/ProTip';
-import Box from '@material-ui/core/Box';
+import SEO from '../components/page/Seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Typography variant="h1" component="h1" gutterBottom>
-      veeck.de
-    </Typography>
     <Box my={4}>
       <p>Welcome to my new Gatsby site.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
       <Link to="/links/">Go to links page</Link>
       <ProTip />
     </Box>
