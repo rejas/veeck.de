c11a8413defb0e13618a83c1fa346ec79facb88e
rejas <github@veeck.de>
2019-12-15 22:50:10 +0100

    Add lang to Helmet, remove Image imports

--- src/templates/travelPage.jsx
+++ src/templates/travelPage.jsx
@@ -3,11 +3,13 @@ import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from '../components/page/Layout';
 import SEO from '../components/page/Seo';
+import { Headline1 } from '../components/Typography';
 
 const TravelTemplate = ({ data: { mdx } }) => {
   return (
     <Layout>
-      <SEO title={mdx.frontmatter.title} />
+      <SEO title={mdx.frontmatter.title} lang={mdx.frontmatter.lang}/>
+      <Headline1>{mdx.frontmatter.title}</Headline1>
       <MDXRenderer>{mdx.body}</MDXRenderer>
     </Layout>
   );
@@ -19,7 +21,8 @@ export const pageQuery = graphql`
       id
       body
       frontmatter {
-        title
+        title,
+          lang
       }
     }
   }
