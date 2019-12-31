32e6819437f60bed787f3b34a2ccb3d859ffc39a
rejas <github@veeck.de>
2019-12-31 12:06:16 +0100

    Use grid on gallery page

--- src/templates/galleryPage.jsx
+++ src/templates/galleryPage.jsx
@@ -2,21 +2,29 @@ import React from 'react';
 import Layout from '../components/page/Layout';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import { Grid } from '@material-ui/core';
 
 const GalleryTemplate = props => {
   const node = props.data.allPhotosYaml.edges[0].node;
 
   return (
     <Layout>
-      <ul>
+      <Grid container spacing={3}>
         {node.images.map((image, index) => {
           return (
-            <li key={`content_item_${index}`}>
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              key={`content_item_${index}`}
+            >
               <Img fluid={image.img.childImageSharp.fluid} />
-            </li>
+            </Grid>
           );
         })}
-      </ul>
+      </Grid>
     </Layout>
   );
 };
