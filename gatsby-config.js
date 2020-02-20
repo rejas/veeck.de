e7a0369dfd4da271edc53b8e7b338c558009effb
rejas <github@veeck.de>
2020-02-20 23:30:16 +0100

    Try out tinacms

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,16 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-plugin-tinacms',
+      options: {
+        sidebar: {
+          hidden: process.env.NODE_ENV === 'production',
+          position: 'displace',
+        },
+        plugins: ['gatsby-tinacms-git', 'gatsby-tinacms-remark'],
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
