cb28d544f86df77dcca8e69c8c63758fc692672b
Veeck <github@veeck.de>
2020-12-14 13:48:05 +0100

    Switch to File System Route API  (#144)
    * Update code to look like migration example
    
    * Convert mdx nodes to file api
    
    * Rename pages to fix slug mismatch
    
    * Update dependencies
    
    * Update yaml code too

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,57 +7,23 @@ const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  // you only want to operate on `Mdx` nodes. If you had content from a
-  // remote CMS you could also check to see if the parent node was a
-  // `File` node here
+  const { createNodeField } = actions;
+
   if (node.internal.type === 'Mdx') {
     const value = createFilePath({ node, getNode });
-    actions.createNodeField({
-      // Name of the field you are adding
+
+    createNodeField({
       name: 'slug',
-      // Individual MDX node
       node,
-      // Keep base path value
       value,
     });
   }
 };
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
-  // Create pages from mdx data
-  const mdxResult = await graphql(`
-    query {
-      allMdx {
-        edges {
-          node {
-            id
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `);
-  if (mdxResult.errors) {
-    reporter.panicOnBuild('🚨ERROR: Loading "createPages" query');
-  }
+  const { createPage } = actions;
+  const galleryPage = path.resolve(`./src/templates/galleryPage.jsx`);
 
-  const posts = mdxResult.data.allMdx.edges;
-  posts.forEach(({ node }) => {
-    actions.createPage({
-      // This is the slug you created before
-      // (or `node.frontmatter.slug`)
-      path: node.fields.slug,
-      // This component will wrap our MDX content
-      component: path.resolve(`./src/templates/mdxPage.jsx`),
-      // You can use the values in this context in
-      // our page layout component
-      context: { id: node.id },
-    });
-  });
-
-  // Create pages from yaml data
   const yamlResult = await graphql(`
     query {
       allPhotosYaml {
@@ -70,16 +36,18 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       }
     }
   `);
+
   if (yamlResult.errors) {
-    reporter.panicOnBuild('🚨ERROR: Loading "createPages" query');
+    reporter.panicOnBuild('🚨 ERROR: Loading "createPages" query');
+    return;
   }
 
   const galleries = yamlResult.data.allPhotosYaml.edges;
-  galleries.forEach(({ node }) => {
-    actions.createPage({
-      path: '/photos/' + node.path,
-      component: path.resolve(`./src/templates/galleryPage.jsx`),
-      context: { id: node.id },
+  galleries.forEach((gallery) => {
+    createPage({
+      path: '/photos/' + gallery.node.path,
+      component: galleryPage,
+      context: { id: gallery.node.id },
     });
   });
 };
