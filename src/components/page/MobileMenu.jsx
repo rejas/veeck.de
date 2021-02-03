6f69996bd054c5ab041c6cc4052edfe4ca1290b5
veeck <veeck@neofonie.de>
2021-02-03 20:40:09 +0100

    Add click handling

--- src/components/page/MobileMenu.jsx
+++ src/components/page/MobileMenu.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import { Box } from '@material-ui/core';
 import { Fab } from 'gatsby-theme-material-ui';
 import MenuIcon from '@material-ui/icons/Menu';
+import Zoom from '@material-ui/core/Zoom';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -13,11 +15,24 @@ const useStyles = makeStyles((theme) => ({
 
 const MobileMenu = () => {
   const classes = useStyles();
+  const [checked, setChecked] = React.useState(false);
+
+  const handleFabClick = (event) => {
+    setChecked(!checked);
+  };
 
   return (
-    <Fab color="primary" aria-label="add" className={classes.root}>
-      <MenuIcon />
-    </Fab>
+    <Box className={classes.root}>
+      <Zoom in={checked}>
+        <Fab color="secondary">
+          <MenuIcon />
+        </Fab>
+      </Zoom>
+
+      <Fab onClick={handleFabClick} color="primary" aria-label="add">
+        <MenuIcon />
+      </Fab>
+    </Box>
   );
 };
 
