1618d50e8b3c00c1be7aee7729db3f7bb30ba306
veeck <veeck@neofonie.de>
2021-05-27 09:48:40 +0200

    Use correct date in rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -166,6 +166,7 @@ module.exports = {
               return allMdx.edges.map((edge) => {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
+                  date: edge.node.frontmatter.last_modified,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   custom_elements: [{ 'content:encoded': edge.node.html }],
