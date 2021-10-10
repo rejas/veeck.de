673039935de60187b011969a6fffbd950aa47713
rejas <github@veeck.de>
2021-10-10 15:31:30 +0200

    Migrate theme

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,5 @@
-import {
-  createTheme,
-  responsiveFontSizes,
-  adaptV4Theme,
-} from '@mui/material/styles';
+import { responsiveFontSizes } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 
 // TODO enhance it like this https://css-tricks.com/a-dark-mode-toggle-with-react-and-themeprovider/
 // maybe switch to something like mentioned here: https://material-ui.com/customization/palette/#user-preference
@@ -15,92 +12,98 @@ if (typeof window !== 'undefined') {
     window.matchMedia('(prefers-color-scheme: dark)').matches;
 }
 
-const theme = createTheme(
-  adaptV4Theme({
-    overrides: {
-      MuiBottomNavigationAction: {
+const theme = createTheme({
+  components: {
+    MuiBottomNavigationAction: {
+      styleOverrides: {
         root: {
           minWidth: 0,
         },
       },
-      MuiDivider: {
+    },
+    MuiDivider: {
+      styleOverrides: {
         root: {
           marginBottom: '1rem',
           marginTop: '1rem',
         },
       },
-      MuiLink: {
+    },
+    MuiLink: {
+      styleOverrides: {
         root: {
           fontWeight: 'bold',
         },
       },
-      MuiList: {
+    },
+    MuiList: {
+      styleOverrides: {
         root: {
           fontSize: '1rem',
         },
       },
     },
-    typography: {
+  },
+  typography: {
+    fontFamily: ['Josefin Sans', 'sans-serif'].join(','),
+    h1: {
+      fontFamily: ['Gilroy', 'sans-serif'].join(','),
+      fontSize: '4rem',
+      fontWeight: 'bold',
+      lineHeight: '1',
+      textAlign: 'center',
+      textTransform: 'uppercase',
+      wordBreak: 'break-word',
+    },
+    h2: {
+      fontFamily: ['Gilroy', 'sans-serif'].join(','),
+      fontSize: '2rem',
+      fontWeight: 'bold',
+      textTransform: 'uppercase',
+    },
+    h3: {
       fontFamily: ['Josefin Sans', 'sans-serif'].join(','),
-      h1: {
-        fontFamily: ['Gilroy', 'sans-serif'].join(','),
-        fontSize: '4rem',
-        fontWeight: 'bold',
-        lineHeight: '1',
-        textAlign: 'center',
-        textTransform: 'uppercase',
-        wordBreak: 'break-word',
-      },
-      h2: {
-        fontFamily: ['Gilroy', 'sans-serif'].join(','),
-        fontSize: '2rem',
-        fontWeight: 'bold',
-        textTransform: 'uppercase',
-      },
-      h3: {
-        fontFamily: ['Josefin Sans', 'sans-serif'].join(','),
-        fontSize: '1.666rem',
-      },
-      h4: {
-        fontFamily: ['Josefin Sans', 'sans-serif'].join(','),
-        fontSize: '1rem',
-        fontWeight: 'bold',
-      },
-      h5: {
-        fontFamily: ['Josefin Sans', 'sans-serif'].join(','),
-        fontWeight: 'bold',
-      },
-      h6: {
-        fontFamily: ['Josefin Sans', 'sans-serif'].join(','),
-      },
-      button: {
-        fontWeight: 'bold',
-      },
-      subtitle1: {
-        fontFamily: ['Josefin Sans', 'sans-serif'].join(','),
-        fontSize: '1.666rem',
-        lineHeight: '1',
-        textAlign: 'center',
-        textTransform: 'lowercase',
-      },
+      fontSize: '1.666rem',
     },
-    palette: {
-      mode: prefersDarkMode ? 'dark' : 'light',
-      primary: {
-        main: prefersDarkMode ? '#fae34b' : '#939597',
-      },
-      secondary: {
-        main: prefersDarkMode ? '#313131' : '#f1f1f1',
-      },
-      error: {
-        main: prefersDarkMode ? '#cf6679' : '#ff443d',
-      },
-      background: {
-        default: prefersDarkMode ? '#3c3c3c' : '#ededed',
-        paper: prefersDarkMode ? '#314151' : '#fcfcfc',
-      },
+    h4: {
+      fontFamily: ['Josefin Sans', 'sans-serif'].join(','),
+      fontSize: '1rem',
+      fontWeight: 'bold',
     },
-  })
-);
+    h5: {
+      fontFamily: ['Josefin Sans', 'sans-serif'].join(','),
+      fontWeight: 'bold',
+    },
+    h6: {
+      fontFamily: ['Josefin Sans', 'sans-serif'].join(','),
+    },
+    button: {
+      fontWeight: 'bold',
+    },
+    subtitle1: {
+      fontFamily: ['Josefin Sans', 'sans-serif'].join(','),
+      fontSize: '1.666rem',
+      lineHeight: '1',
+      textAlign: 'center',
+      textTransform: 'lowercase',
+    },
+  },
+  palette: {
+    mode: prefersDarkMode ? 'dark' : 'light',
+    primary: {
+      main: prefersDarkMode ? '#fae34b' : '#939597',
+    },
+    secondary: {
+      main: prefersDarkMode ? '#313131' : '#f1f1f1',
+    },
+    error: {
+      main: prefersDarkMode ? '#cf6679' : '#ff443d',
+    },
+    background: {
+      default: prefersDarkMode ? '#3c3c3c' : '#ededed',
+      paper: prefersDarkMode ? '#314151' : '#fcfcfc',
+    },
+  },
+});
 
 export default responsiveFontSizes(theme);
