360dc289c6ed2d45d6e288240de3710f4eefe8e9
veeck <michael@veeck.de>
2023-03-07 11:04:58 +0100

    Replace __dirname

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -1,4 +1,9 @@
+import path from 'path';
 import remarkUnwrapImages from 'remark-unwrap-images';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config = {
   siteMetadata: {
