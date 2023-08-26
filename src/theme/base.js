e53a781322e9d7d2829dd5686ea1c5176568d0ca
veeck <michael.veeck@nebenan.de>
2023-08-26 23:14:37 +0200

    Switch font

--- src/theme/base.js
+++ src/theme/base.js
@@ -47,11 +47,11 @@ const baseTheme = createTheme({
   typography: {
     fontFamily: ['Josefin Sans', 'sans-serif'].join(','),
     h1: {
-      fontFamily: ['Dyna Puff', 'serif'].join(','),
+      fontFamily: ['Bricolage Grotesque', 'serif'].join(','),
       fontSize: '4rem',
     },
     h2: {
-      fontFamily: ['Dyna Puff', 'serif'].join(','),
+      fontFamily: ['Bricolage Grotesque', 'serif'].join(','),
       fontSize: '3rem',
     },
     h3: {
