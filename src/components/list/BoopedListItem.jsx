ca3d2b3b7682347954ff03915affd20661e5bee6
veeck <gitkraken@veeck.de>
2024-11-29 13:11:44 +0100

    Use ListItemButton

--- src/components/list/BoopedListItem.jsx
+++ src/components/list/BoopedListItem.jsx
@@ -1,4 +1,4 @@
-import { ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { Link } from 'gatsby-theme-material-ui';
 import * as React from 'react';
 import { animated } from 'react-spring';
@@ -10,14 +10,14 @@ const BoopedListItem = ({ post }) => {
   const [style, trigger] = useBoop({ rotation: 20, timing: 200 });
 
   return (
-    <ListItem button component={Link} to={post.fields.slug} onMouseEnter={trigger}>
+    <ListItemButton component={Link} to={post.fields.slug} onMouseEnter={trigger}>
       <ListItemIcon>
         <animated.span style={style}>
           <CategoryIcon category={post.frontmatter.subcategory} color="primary" />
         </animated.span>
       </ListItemIcon>
       <ListItemText primary={post.frontmatter.title} secondary={post.excerpt} />
-    </ListItem>
+    </ListItemButton>
   );
 };
 
