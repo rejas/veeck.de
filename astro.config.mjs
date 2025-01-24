9e30696a8dca30817272ab9c88e97b3c2f42f75b
rejas <github@veeck.de>
2025-01-24 21:56:08 +0100

    Add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
 });
