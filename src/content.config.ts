095441ba1bf168bed880526f5c16a9b767c3ef36
veeck <gitkraken@veeck.de>
2025-01-28 11:42:38 +0100

    Start converting travel diaries

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,26 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
+const preDefinedCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.coerce.date(),
+    tags: z.array(z.string()),
+    img: z.string(),
+    img_alt: z.string().optional(),
+  }),
+});
 export const collections = {
-  projects: defineCollection({
+  projects: {
+    ...preDefinedCollection,
     // Load Markdown files in the src/content/projects directory.
     loader: glob({ base: './src/content/projects', pattern: '**/*.mdx' }),
-    schema: z.object({
-      title: z.string(),
-      description: z.string(),
-      publishDate: z.coerce.date(),
-      tags: z.array(z.string()),
-      img: z.string(),
-      img_alt: z.string().optional(),
-    }),
-  }),
+  },
+  // copy projects into a travel object and replace the loader property
+  travels: {
+    ...preDefinedCollection,
+    // Load Markdown files in the src/content/travel directory.
+    loader: glob({ base: './src/content/travels', pattern: '**/*.mdx' }),
+  },
 };
