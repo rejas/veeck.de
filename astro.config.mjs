ae82251130c7c1a36176cb506ac423d36fcd4ea3
veeck <gitkraken@veeck.de>
2025-02-08 21:56:54 +0100

    Add sitemap and robots.txt, Cleanup frontpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@
 import mdx from '@astrojs/mdx';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import yaml from '@rollup/plugin-yaml';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://veeck.de',
 	build: {
 		// Example: Generate `page.html` instead of `page/index.html` during build.
 		format: 'preserve',
 	},
-	integrations: [mdx(), react()],
+	integrations: [mdx(), react(), sitemap()],
 	adapter: netlify(),
 	vite: {
 		plugins: [yaml()],
