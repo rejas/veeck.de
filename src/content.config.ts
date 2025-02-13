c2196aa9fe66b9455258d0a7905f2acaa18797e5
Veeck <github@veeck.de>
2025-02-13 14:05:18 +0100

    Astro (#234)
    * add astro portfolio template
    
    * move infrastructure files
    
    * Move content around
    
    * bump astro dep
    
    * update branding
    
    * add astro mdx
    
    * Start converting stuff
    
    * Add react integration
    
    * Finish moving projects pages
    
    * add netlify adapter
    
    remove gatsby configs
    
    * add netlify config
    
    * rename work -> projects
    
    * Start converting travel diaries
    
    * Remove ContactCTA, update icons and pills
    
    * add lang to site, fix some links
    
    * update prettier for astro
    
    * More frontmatter cleanups and rework
    
    * cleanup tags, add last_modified to page
    
    * move travel pages
    
    * fix last_modified spelling in frontmatter
    
    * use heading and lead
    
    * Cleanup frontpage
    
    * start converting galleries
    
    * show gallery images
    
    but no popups yet
    
    * cleanups
    
    * add blog posts
    
    * fix url building
    
    * Add sitemap and robots.txt, Cleanup frontpage
    
    * Add eslitn config, fix build, bump version
    
    * cleanups
    
    * updated blog tags
    
    * Update CHANGELOG, LICENSE and dependencies
    
    * add new blog post about switching
    
    ---------
    
    Co-authored-by: veeck <gitkraken@veeck.de>

---
+++ src/content.config.ts
@@ -0,0 +1,60 @@
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
+
+const preDefinedCollection = defineCollection({
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string().optional(), // TODO cleanup description / add leading
+			first_published: z.coerce.date(),
+			last_modified: z.coerce.date().optional(),
+			tags: z.array(z.string()),
+			heading: z.string().optional(),
+			img: image(),
+			img_alt: z.string().optional(), // TODO alt text in frontmatter of pages
+			lang: z.string(),
+			lead: z.string().optional(),
+		}),
+});
+export const collections = {
+	galleries: defineCollection({
+		schema: ({ image }) =>
+			z.object({
+				title: z.string(),
+				last_modified: z.coerce.date(),
+				img: image(),
+				img_alt: z.string().optional(), // TODO alt text in frontmatter of yamls
+				lead: z.string().optional(),
+				images: z.array(
+					z.object({
+						caption: z.string(),
+						img: image(),
+					}),
+				),
+			}),
+		// Load YAML files in the src/content/galleries directory.
+		loader: glob({ base: './src/content/galleries', pattern: '**/*.yaml' }),
+	}),
+	blog: {
+		schema: () =>
+			z.object({
+				title: z.string(),
+				first_published: z.coerce.date(),
+				lang: z.string(),
+				last_modified: z.coerce.date().optional(),
+				tags: z.array(z.string()),
+			}),
+		// Load Markdown files in the src/content/projects directory.
+		loader: glob({ base: './src/content/blog', pattern: '**/*.mdx' }),
+	},
+	projects: {
+		...preDefinedCollection,
+		// Load Markdown files in the src/content/projects directory.
+		loader: glob({ base: './src/content/projects', pattern: '**/*.mdx' }),
+	},
+	travels: {
+		...preDefinedCollection,
+		// Load Markdown files in the src/content/travels directory.
+		loader: glob({ base: './src/content/travels', pattern: '**/*.mdx' }),
+	},
+};
---
+++ src_old/components/navigation/MenuItem.jsx
@@ -0,0 +1,56 @@
+import { styled } from '@mui/material/styles';
+import { Link } from 'gatsby-theme-material-ui';
+import { Button } from 'gatsby-theme-material-ui';
+import PropTypes from 'prop-types';
+import * as React from 'react';
+import { animated } from 'react-spring';
+import useSound from 'use-sound';
+
+import boopSfx from '../../content/sounds/tock.mp3';
+import { useBoop } from '../../hooks/use-boop';
+import CategoryIcon from '../icons/CategoryIcon';
+
+const Headline5Styled = styled(Button)(({ theme }) => ({
+  margin: '0 auto 1rem',
+
+  [theme.breakpoints.up('md')]: {
+    padding: '0.25rem 2rem',
+    width: '100%',
+  },
+}));
+
+const CategoryIconStyled = styled(CategoryIcon)(({ theme }) => ({}));
+
+const LinkStyled = styled(Link)(({ theme }) => ({
+  textTransform: 'uppercase',
+  margin: 0,
+  padding: `${theme.spacing(0.5)} 0 0 ${theme.spacing(1)}`,
+  flexShrink: 0,
+
+  '&:hover': {
+    textDecoration: 'none',
+  },
+}));
+
+const MenuItem = ({ link }) => {
+  const [style, trigger] = useBoop({ rotation: 20, timing: 200 });
+  style.display = 'flex';
+  const [play] = useSound(boopSfx);
+
+  return (
+    <Headline5Styled onMouseEnter={trigger} key={link.name} color="secondary" variant="contained">
+      <animated.span style={style}>
+        <CategoryIconStyled category={link.icon} color="primary" />
+      </animated.span>
+      <LinkStyled onClick={play} color="inherit" to={link.url}>
+        {link.name}
+      </LinkStyled>
+    </Headline5Styled>
+  );
+};
+
+MenuItem.propTypes = {
+  link: PropTypes.object.isRequired,
+};
+
+export default MenuItem;
