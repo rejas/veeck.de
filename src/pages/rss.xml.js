6a1a38a38910e1c2980f5d36f781ffb26fcd3760
veeck <gitkraken@veeck.de>
2025-02-17 23:00:49 +0100

    add real rss data

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,37 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
+// @see https://docs.astro.build/en/recipes/rss/
 export async function GET(context) {
 	const blog = await getCollection('blog');
-	return rss({
-		title: 'Buzz’s Blog',
-		description: 'A humble Astronaut’s guide to the stars',
-		site: context.site,
-		items: blog.map((post) => ({
+	const projects = await getCollection('projects');
+	const travels = await getCollection('travels');
+
+	const items = blog
+		.map((post) => ({
 			title: post.data.title,
-			pubDate: post.data.pubDate,
-			description: post.data.description,
-			// Compute RSS link from post `id`
-			// This example assumes all posts are rendered as `/blog/[id]` routes
-			link: `/blog/${post.id}/`,
-		})),
+			pubDate: post.data.first_published,
+			link: `/blog/${post.slug}/`,
+		}))
+		.concat(
+			projects.map((post) => ({
+				title: post.data.title,
+				pubDate: post.data.first_published,
+				link: `/projects/${post.slug}/`,
+			})),
+		)
+		.concat(
+			travels.map((post) => ({
+				title: post.data.title,
+				pubDate: post.data.first_published,
+				link: `/projects/${post.slug}/`,
+			})),
+		);
+
+	return rss({
+		title: 'Veeck´s Homepage',
+		description: 'Computerschlampe, Hoffotograf, Terrorpoet',
+		site: context.site,
+		items: items,
 	});
 }
