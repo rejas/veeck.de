55846ab3a890a95240536ab4f79da0bdada2fd38
veeck <gitkraken@veeck.de>
2025-10-19 16:16:43 +0200

    add og package

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,23 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import yaml from '@rollup/plugin-yaml';
+import opengraphImages, { presets } from 'astro-opengraph-images';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://veeck.de',
-	integrations: [mdx(), react(), sitemap()],
+	integrations: [
+		mdx(),
+		react(),
+		sitemap(),
+		opengraphImages({
+			options: {
+				verbose: true,
+			},
+			render: presets.blackAndWhite,
+		}),
+	],
 	adapter: netlify(),
 	vite: {
 		plugins: [yaml()],
