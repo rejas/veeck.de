df7ebb49208c561aea5671041ab1c1302a0a1bcc
Veeck <github@veeck.de>
2025-10-19 19:12:08 +0200

    Add og meta tags with simple layout (#296)
    * add og package
    
    * add missing font option
    
    * bump deps
    
    ---------
    
    Co-authored-by: veeck <gitkraken@veeck.de>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,31 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import yaml from '@rollup/plugin-yaml';
+import opengraphImages, { presets } from 'astro-opengraph-images';
 import { defineConfig } from 'astro/config';
+import * as fs from 'fs';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://veeck.de',
-	integrations: [mdx(), react(), sitemap()],
+	integrations: [
+		mdx(),
+		react(),
+		sitemap(),
+		opengraphImages({
+			options: {
+				fonts: [
+					{
+						name: 'Josefin Sans',
+						weight: 400,
+						style: 'normal',
+						data: fs.readFileSync('node_modules/@fontsource/josefin-sans/files/josefin-sans-latin-400-normal.woff'),
+					},
+				],
+			},
+			render: presets.blackAndWhite,
+		}),
+	],
 	adapter: netlify(),
 	vite: {
 		plugins: [yaml()],
